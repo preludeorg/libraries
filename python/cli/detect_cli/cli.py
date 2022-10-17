1ca031ce95304bb65f79a96245649a20dd126505
privateducky <david@prelude.org>
2022-10-16 22:01:55 -0400

    moore

--- python/cli/detect_cli/cli.py
+++ python/cli/detect_cli/cli.py
@@ -1,7 +1,7 @@
 import click
 from detect_cli.views.account import account
 from detect_cli.views.configure import configure
-from detect_cli.views.endpoints import endpoints
+from detect_cli.views.endpoint import endpoint
 from detect_cli.views.database import database
 from detect_cli.views.schedule import schedule
 from detect_sdk.models.account import Account
@@ -16,7 +16,7 @@ def cli(ctx, profile):
 
 cli.add_command(account)
 cli.add_command(configure)
-cli.add_command(endpoints)
+cli.add_command(endpoint)
 cli.add_command(schedule)
 cli.add_command(database)
 
