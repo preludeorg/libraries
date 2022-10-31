bd1394ebc890a5e2076bc8854f5cf592e541b3b6
privateducky <david@prelude.org>
2022-10-31 06:50:37 -0400

    adding compute proxyg

--- python/cli/prelude_cli/views/compute.py
+++ python/cli/prelude_cli/views/compute.py
@@ -2,6 +2,7 @@ import click
 from prelude_cli.views.shared import handle_api_error
 from prelude_sdk.controllers.compute_controller import ComputeController
 from prelude_sdk.models.codes import Colors
+from rich import print_json
 
 
 @click.group()
@@ -20,6 +21,26 @@ def describe_server(controller):
     if private_ip:
         click.secho(f'Your Compute server is {private_ip}', fg=Colors.GREEN.value)
     print('Compute server booting up. Please try again in 30s.')
+
+
+@compute.command('test-code')
+@click.argument('name')
+@click.pass_obj
+@handle_api_error
+def test_code(controller, name):
+    """ Test a code file """
+    steps = controller.compute_proxy(route='test', name=name)
+    print_json(steps)
+
+
+@compute.command('publish-code')
+@click.argument('name')
+@click.pass_obj
+@handle_api_error
+def publish_ttp(controller, name):
+    """ Save compiled binaries to storage """
+    controller.compute_proxy(route='publish', name=name)
+    print(f'{name} published')
 
 
 @compute.command('create-url')
