56b4e91691bfbb439c60e54b2071ee714c4e647f
makk94 <mahina.kaholokula@prelude.org>
2022-11-04 07:27:26 -0700

    os uname doesnt exist on windows

--- python/probe/detect_probe/presto.py
+++ python/probe/detect_probe/presto.py
@@ -1,5 +1,5 @@
 import asyncio
-import os
+import platform
 import re
 import stat
 import subprocess
@@ -7,7 +7,7 @@ import tempfile
 import os.path
 from urllib import request
 
-DOS = f'{os.uname().sysname}-{os.uname().machine}'.lower()
+DOS = f'{platform.system()}-{platform.machine()}'.lower()
 UUID = re.compile('[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}')
 
 
