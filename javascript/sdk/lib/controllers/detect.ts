773b51785ae30701d45c8b62add444301cb0a71f
Kenrick Beckett <760470+kenrick@users.noreply.github.com>
2022-11-07 08:11:56 -0700

    add list tags and changed to /queue from /activity (#60)

--- javascript/sdk/lib/controllers/detect.ts
+++ javascript/sdk/lib/controllers/detect.ts
@@ -3,7 +3,7 @@ import {
   AccountActivity,
   AccountQueue,
   RequestOptions,
-  RunCode,
+  EnableTest,
 } from "../types";
 
 export default class DetectController {
@@ -13,6 +13,7 @@ export default class DetectController {
     this.#client = client;
   }
 
+  /** Register (or re-register) an endpoint to your account */
   async registerEndpoint(
     { name, tag = "" }: { name: string; tag?: string },
     options: RequestOptions
@@ -26,19 +27,6 @@ export default class DetectController {
     return response.text();
   }
 
-  async accountActivity(
-    { days = 7 }: { days?: number },
-    options: RequestOptions
-  ) {
-    const response = await this.#client.requestWithAuth("/account/activity", {
-      method: "GET",
-      body: JSON.stringify({ days }),
-      ...options,
-    });
-
-    return (await response.json()) as AccountActivity;
-  }
-
   async printQueue(options: RequestOptions) {
     const response = await this.#client.requestWithAuth("/account/queue", {
       method: "GET",
@@ -48,18 +36,66 @@ export default class DetectController {
     return (await response.json()) as AccountQueue;
   }
 
-  async enableTest(test: string, runCode: RunCode, options: RequestOptions) {
-    await this.#client.requestWithAuth(`/account/activation/${test}`, {
+  /** Enable a test so endpoints will start running it */
+  async enableTest(
+    { test, runCode, tags }: EnableTest,
+    options: RequestOptions
+  ) {
+    await this.#client.requestWithAuth(`/account/queue/${test}`, {
       method: "POST",
-      body: JSON.stringify({ run_code: runCode }),
+      body: JSON.stringify({ run_code: runCode, tags }),
       ...options,
     });
   }
 
+  /** Disable a test so endpoints will stop running it */
   async disableTest(test: string, options: RequestOptions) {
-    await this.#client.requestWithAuth(`/account/activation/${test}`, {
+    await this.#client.requestWithAuth(`/account/queue/${test}`, {
       method: "DELETE",
       ...options,
     });
+  }
+
+  /** Get report for an Account */
+  async describeActivity(
+    { days = 7, ident }: { days?: number; ident?: string },
+    options: RequestOptions
+  ) {
+    let route = !!ident ? `/${ident}` : "";
+
+    const response = await this.#client.requestWithAuth(
+      `/account/report${route}`,
+      {
+        method: "GET",
+        body: JSON.stringify({ days }),
+        ...options,
+      }
+    );
+
+    return (await response.json()) as AccountActivity;
+  }
+
+  /** Generate a redirect URL to a data dump */
+  async exportReport({ days = 7 }: { days?: number }, options: RequestOptions) {
+    const response = await this.#client.requestWithAuth(
+      "/account/report-export",
+      {
+        method: "GET",
+        body: JSON.stringify({ days }),
+        ...options,
+      }
+    );
+
+    return (await response.json()) as string;
+  }
+
+  /** Get all tags associated to an Account */
+  async listTags(options: RequestOptions) {
+    const response = await this.#client.requestWithAuth("/account/tag", {
+      method: "GET",
+      ...options,
+    });
+
+    return await response.json();
   }
 }
