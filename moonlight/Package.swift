35958161393a6c55d789202ee832aa2e5530466c
privateducky <david@prelude.org>
2022-11-09 11:34:06 -0500

    uploading moonlight code

---
+++ moonlight/Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Mini",
+    dependencies: [],
+    targets: [
+        .executableTarget(
+            name: "Mini",
+            dependencies: []),
+    ]
+)
