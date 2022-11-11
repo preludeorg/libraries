5560ccbda8670738a90066434fac638baab22b3c
FM <mireaulf@gmail.com>
2022-11-10 21:59:16 -0500

    Map question id to name (#75)

--- python/cli/prelude_cli/views/detect.py
+++ python/cli/prelude_cli/views/detect.py
@@ -1,5 +1,6 @@
 import click
 from prelude_cli.views.shared import handle_api_error
+from prelude_sdk.controllers.build_controller import BuildController
 from prelude_sdk.controllers.detect_controller import DetectController
 from prelude_sdk.models.codes import Colors, RunCode
 from rich import print_json
@@ -66,6 +67,8 @@ def queue(controller):
 def describe_activity(controller, days):
     """ View report for my Account """
     raw = controller.describe_activity(days=days)
+    build = BuildController(account=controller.account)
+    tests = {row['id']: row['question'] for row in build.list_tests()}
 
     report = Table()
     report.add_column('test')
@@ -81,7 +84,7 @@ def describe_activity(controller, days):
         failed = test.get('FAILED', 0)
         error = test.get('ERROR', 0)
         volume = ok + stopped + failed + error
-        report.add_row(i, str(volume), str(round((ok / volume) * 100)), str(round((stopped / volume) * 100)),
+        report.add_row(tests[i], str(volume), str(round((ok / volume) * 100)), str(round((stopped / volume) * 100)),
                        str(round((failed / volume) * 100)), str(round((error / volume) * 100)))
 
     console = Console()
