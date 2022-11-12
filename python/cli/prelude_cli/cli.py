a5b96eac2b42dea956902c40709eb5d18ff83a7a
david <49954156+privateducky@users.noreply.github.com>
2022-11-12 17:24:57 -0500

    consolidating build/compute sections (#76)

--- python/cli/prelude_cli/cli.py
+++ python/cli/prelude_cli/cli.py
@@ -1,7 +1,6 @@
 import click
 from prelude_cli.views.iam import iam
 from prelude_cli.views.configure import configure
-from prelude_cli.views.compute import compute
 from prelude_cli.views.build import build
 from prelude_cli.views.detect import detect
 from prelude_sdk.models.account import Account
@@ -18,7 +17,6 @@ cli.add_command(iam)
 cli.add_command(configure)
 cli.add_command(detect)
 cli.add_command(build)
-cli.add_command(compute)
 
 
 if __name__ == '__main__':
