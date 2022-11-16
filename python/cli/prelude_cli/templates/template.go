7eb84eb264fad0089b40545f6d6e70f2a6504f4d
makk94 <mahina.kaholokula@prelude.org>
2022-11-16 10:45:56 -0800

    update

--- python/cli/prelude_cli/templates/template.go
+++ python/cli/prelude_cli/templates/template.go
@@ -1,26 +1,25 @@
 package main
 
-import "fmt"
-
 import (
-	"os"
+    "fmt"
+    "os"
 )
 
 func test() {
     fmt.Println("Run test")
-	os.Exit(100)
+    os.Exit(100)
 }
 
 func cleanup() {
-    fmt.Println("Clean up")
-	os.Exit(100)
+    fmt.Println("Clean up")https://github.com/preludeorg/libraries/pull/new/mk
+    os.Exit(100)
 }
 
 func main() {
-	args := os.Args[1:]
-	if len(args) > 0 {
-		cleanup()
-	} else {
-		test()
-	}
+    args := os.Args[1:]
+    if len(args) > 0 {
+        cleanup()
+    } else {
+        test()
+    }
 }
