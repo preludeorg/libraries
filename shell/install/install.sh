c8a27b037105be915d0163e47c32e240a22927d5
Alex Manners <alexandermanners@gmail.com>
2022-11-17 12:19:23 -0500

    updating install script

--- shell/install/install.sh
+++ shell/install/install.sh
@@ -11,16 +11,24 @@ function usage {
     echo
     echo "Usage: $(basename $0) [-h] -n PROBE_NAME" 2>&1
     echo
-    echo '  -h                 Shows Usage'
-    echo "  -n PROBE_NAME      Probe Name; Default: ${PROBE_NAME}"
+    echo '  -h                          Shows Usage'
+    echo "  -n PROBE_NAME               Probe Name; Default: ${PROBE_NAME}"
+    echo "  -a PRELUDE_ACCOUNT_ID       Prelude Account Id; ${PRELUDE_ACCOUNT_ID}"
+    echo "  -s PRELUDE_ACCOUNT_SECRET   Prelude Account Secret; ${PRELUDE_ACCOUNT_SECRET}"
     echo
     exit
 }
-optstring="n:h"
+optstring="n:a:s:h"
 while getopts ${optstring} arg; do
     case ${arg} in
         n)
             PROBE_NAME="${OPTARG}"
+            ;;
+        a)
+            PRELUDE_ACCOUNT_ID="${OPTARG}"
+            ;;
+        s)
+            PRELUDE_ACCOUNT_SECRET="${OPTARG}"
             ;;
         h)
             usage
@@ -36,16 +44,16 @@ done
 register_new_endpoint() {
     echo "[+] Provisioning Detect Endpoint Token..."
     local _token_url="${PRELUDE_API}/account/endpoint"
-    ENDPOINT_TOKEN=$(curl -s -X POST -H "account:${PRELUDE_ACCOUNT_ID}" -H "token:${PRELUDE_ACCOUNT_SECRET}" -H "Content-Type: application/json" -d "{\"id\":\"${HOST}\",\"tag\":\"darwin\"}"  "${_token_url}")
+    _hostname=$(hostname)
+    ENDPOINT_TOKEN=$(curl -s -X POST -H "account:${PRELUDE_ACCOUNT_ID}" -H "token:${PRELUDE_ACCOUNT_SECRET}" -H "Content-Type: application/json" -d "{\"id\":\"${_hostname}\",\"tag\":\"darwin\"}"  "${_token_url}")
     export ENDPOINT_TOKEN
 }
 
 download_probe () {
     local _tmp_dir=$1
-    local _probe_url="${PRELUDE_API}/download/${PROBE_NAME}?dos=${DOS}"
-
+    local _probe_url="${PRELUDE_API}/download/${PROBE_NAME}"
     echo "[+] Downloading Probe..."
-    curl -s -X GET -L -H "token:${ENDPOINT_TOKEN}" "${_probe_url}" > "${_tmp_dir}/${PROBE_NAME}"
+    curl -o "${_tmp_dir}/${PROBE_NAME}" -s -X GET -L -H "token:${ENDPOINT_TOKEN}" -H"dos:${DOS}" "${_probe_url}"
 
     test -r "${_tmp_dir}/${PROBE_NAME}" || {
         echo "[!] Detect failed to download!" >&2
@@ -55,6 +63,8 @@ download_probe () {
 
 install_darwin_plist () {
     local _plist_file_path=$1
+    local _user=$2
+    local _app_dir=$3
     echo "[+] Generate PLIST"
 
     [[ ! -d "$(dirname $_plist_file_path)" ]] && mkdir "$(dirname $_plist_file_path)"
@@ -67,7 +77,7 @@ install_darwin_plist () {
         <key>Label</key>
         <string>Prelude Detect</string>
         <key>UserName</key>
-        <string>${running_user}</string>
+        <string>${_user}</string>
         <key>EnvironmentVariables</key>
         <dict>
             <key>PRELUDE_TOKEN</key>
@@ -75,7 +85,7 @@ install_darwin_plist () {
         </dict>
         <key>ProgramArguments</key>
         <array>
-            <string>${app_dir}/${PROBE_NAME}</string>
+            <string>${_app_dir}/${PROBE_NAME}</string>
         </array>
         <key>KeepAlive</key>
         <true/>
@@ -92,8 +102,8 @@ EOF
 
 install_darwin() {
   local _running_user="${USER}"
-  local _plist_path="/Users/${running_user}/Library/Launchprobes/com.preludesecurity.detect.plist"
-  local _app_dir="/Users/${running_user}/.prelude/bin"
+  local _plist_path="/Users/${_running_user}/Library/LaunchAgents/com.preludesecurity.detect.plist"
+  local _app_dir="/Users/${_running_user}/.prelude/bin"
   local _app_tmp="/tmp/prelude"
   _primary_group=$(id -gn)
 
@@ -103,7 +113,7 @@ install_darwin() {
   register_new_endpoint
   download_probe "$_app_tmp"
   install -o "${_running_user}" -g "${_primary_group}" -m 755 "${_app_tmp}/${PROBE_NAME}" "${_app_dir}/${PROBE_NAME}"
-  install_darwin_plist "$_plist_path"
+  install_darwin_plist "$_plist_path" "$_running_user" "$_app_dir"
   unset ENDPOINT_TOKEN
   echo "[*] Cleaning up tmp directory"
   rm -rf "${_app_tmp}"
@@ -111,9 +121,9 @@ install_darwin() {
 }
 
 echo "[+] Detect setup started"
-if [[ -z $PRELUDE_ACCOUNT_ID || -z $PRElUDE_ACCOUNT_SECRET ]]
+if [[ -z $PRELUDE_ACCOUNT_ID || -z $PRELUDE_ACCOUNT_SECRET ]];
 then
-    echo "[!] Failed to provide account credentials. Make sure you export PRELUDE_ACCOUNT_ID and PRELUDE_ACCOUNT_SECRET"
+    echo "[!] Failed to provide account credentials. Make sure you provide PRELUDE_ACCOUNT_ID and PRELUDE_ACCOUNT_SECRET"
     usage
 fi
 
