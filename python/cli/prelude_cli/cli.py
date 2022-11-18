b9e427a4d122b93ef6cc9d6e4fcd419f6ffb6ec8
makk94 <mahina.kaholokula@prelude.org>
2022-11-18 12:52:31 -0800

    dont wrap newline

--- python/cli/prelude_cli/cli.py
+++ python/cli/prelude_cli/cli.py
@@ -8,7 +8,7 @@ from prelude_sdk.models.account import Account
 
 @click.group()
 @click.pass_context
-@click.option('--profile', default='default', help='detect keychain profile', show_default=True)
+@click.option('--profile', default='default', help='prelude keychain profile', show_default=True)
 def cli(ctx, profile):
     ctx.obj = Account(profile=profile)
 
