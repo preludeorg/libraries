70a1b4c12610c18b2c3aa2ae1b0470e540916819
Patrick Flaherty <patrick.flaherty@prelude.org>
2022-11-18 11:31:15 -0500

    Initial macos build - pt4

--- swift/probe/Package.swift
+++ swift/probe/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "moonlight",
-    path: "Sources",
+    path: "./Sources",
     dependencies: [],
     targets: [
         .executableTarget(
