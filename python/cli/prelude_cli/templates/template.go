7402b68fa853e5a1f88d470af9d4a5b865a14588
makk94 <mahina.kaholokula@prelude.org>
2022-11-23 08:57:46 -0800

    add go template

---
+++ python/cli/prelude_cli/templates/template.go
@@ -0,0 +1,30 @@
+/*
+NAME: $NAME
+QUESTION: $QUESTION
+CREATED: $CREATED
+*/
+package main
+
+import (
+    "fmt"
+    "os"
+)
+
+func test() {
+    fmt.Println("Run test")
+    os.Exit(100)
+}
+
+func clean() {
+    fmt.Println("Clean up")
+    os.Exit(100)
+}
+
+func main() {
+    args := os.Args[1:]
+    if len(args) > 0 {
+        clean()
+    } else {
+        test()
+    }
+}
