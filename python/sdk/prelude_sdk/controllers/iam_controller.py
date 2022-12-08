9db502c0a6f589daafa2980fc062866ccd46e1fa
Kenrick Beckett <760470+kenrick@users.noreply.github.com>
2022-12-08 12:26:19 -0700

    add update handle cmd to cli (#155)
    * add update handle cmd to cli
    
    * i dont think this will work :)
    
    * fix
    
    * add updateAccount method to js sdk
    
    * update
    
    * remove user update options
    
    * remove user update options
    
    Co-authored-by: makk94 <mahina.kaholokula@prelude.org>

--- python/sdk/prelude_sdk/controllers/iam_controller.py
+++ python/sdk/prelude_sdk/controllers/iam_controller.py
@@ -46,15 +46,6 @@ class IAMController:
         raise Exception(res.text)
 
     @verify_credentials
-    def update_token(self, token):
-        res = requests.put(f'{self.account.hq}/account', headers=self.account.headers, json=dict(token=token))
-        if res.status_code != 200:
-            raise Exception(res.text)
-        cfg = self.account.read_keychain_config()
-        cfg[self.account.profile]['token'] = token
-        self.account.write_keychain_config(cfg)
-
-    @verify_credentials
     def purge_account(self):
         res = requests.delete(f'{self.account.hq}/account/purge', headers=self.account.headers)
         if not res.status_code == 200:
