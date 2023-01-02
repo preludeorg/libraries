51a0aba5b93189fa3d5a8f1a6bcd61bd0431c6f4
Alex Manners <alexandermanners@gmail.com>
2023-01-02 16:24:17 -0500

    Refactor to use the new backend API calls

--- javascript/sdk/lib/controllers/build.ts
+++ javascript/sdk/lib/controllers/build.ts
@@ -16,18 +16,10 @@ export default class BuildController {
     return (await response.json()) as Test[];
   }
 
-  async getTest(id: string, options: RequestOptions = {}) {
-    const response = await this.#client.requestWithAuth(`/build/tests/${id}`, {
-      ...options,
-    });
-
-    return (await response.json()) as string[];
-  }
-
-  async createTest(id: string, question: string, options: RequestOptions = {}) {
+  async createTest(id: string, truth: string, code: string, options: RequestOptions = {}) {
     await this.#client.requestWithAuth(`/build/tests`, {
       method: "POST",
-      body: JSON.stringify({ id, question }),
+      body: JSON.stringify({ id, truth, code }),
       ...options,
     });
   }
@@ -39,41 +31,26 @@ export default class BuildController {
     });
   }
 
-  async createVariant(
-    name: string,
-    code: string,
-    options: RequestOptions = {}
-  ) {
-    await this.#client.requestWithAuth(`/build/variant/${name}`, {
-      method: "POST",
-      body: JSON.stringify({ code }),
-      ...options,
-    });
-  }
-
-  async deleteVariant(name: string, options: RequestOptions = {}) {
-    await this.#client.requestWithAuth(`/build/variant/${name}`, {
-      method: "DELETE",
-      ...options,
-    });
-  }
-
-  async getVariant(name: string, options: RequestOptions = {}) {
+  async downloadTest(name: string, options: RequestOptions = {}) {
     const response = await this.#client.requestWithAuth(
-      `/build/variant/${name}`,
-      {
-        ...options,
-      }
+        `/build/source/${name}`,
+        options
     );
-
     return response.text();
   }
 
-  async createURL(name: string, options: RequestOptions = {}) {
-    const response = await this.#client.requestWithAuth(`/build/${name}/url`, {
+  async uploadTest(name: string, code: string, options: RequestOptions = {}) {
+    await this.#client.requestWithAuth(`/build/source/${name}`, {
+      method: "POST",
+      body: JSON.stringify({ code }),
+      ...options
+    });
+  }
+
+  async createURL(vst: string, options: RequestOptions = {}) {
+    const response = await this.#client.requestWithAuth(`/build/${vst}/url`, {
       ...options,
     });
-
     return (await response.json()) as { url: string };
   }
 
@@ -85,20 +62,5 @@ export default class BuildController {
     });
 
     return (await response.json()) as ComputeResult[];
-  }
-
-  async verifiedTests(options: RequestOptions = {}) {
-    const response = await this.#client.requestWithAuth(`/build/verified`, {
-      ...options,
-    });
-
-    return (await response.json()) as string[];
-  }
-
-  async deleteVerified(name: string, options: RequestOptions = {}) {
-    await this.#client.requestWithAuth(`/build/verified/${name}`, {
-      method: "DELETE",
-      ...options,
-    });
   }
 }
