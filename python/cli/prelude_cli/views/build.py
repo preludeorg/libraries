12d6de2bc240fbe0a6a704ca2fd9d309f62253b8
makk94 <mahina.kaholokula@prelude.org>
2023-01-03 12:31:05 -0800

    rename name to filename

--- python/cli/prelude_cli/views/build.py
+++ python/cli/prelude_cli/views/build.py
@@ -27,8 +27,8 @@ def clone(controller):
     """ Download all tests to your local environment """
     Path('prelude').mkdir(exist_ok=True)
     for test in controller.list_tests():
-        code = controller.download_test(name=test['name'])
-        with open(f'prelude/{test["name"]}', 'wb') as test_code:
+        code = controller.download_test(filename=test['filename'])
+        with open(f'prelude/{test["filename"]}', 'wb') as test_code:
             test_code.write(code)
         click.secho(f'Cloned {test["id"]}')
     click.secho('Project cloned successfully', fg=Colors.GREEN.value)
@@ -55,7 +55,7 @@ def create_test(controller, rule):
     template = template.replace('$NAME', basename)
     template = template.replace('$RULE', rule)
     template = template.replace('$CREATED', str(datetime.now()))
-    controller.upload_test(name=basename, code=template)
+    controller.upload_test(filename=basename, code=template)
 
     with open(basename, 'w') as test_code:
         test_code.write(template)
@@ -80,7 +80,7 @@ def delete_test(controller, test_id):
 def save_test(controller, path):
     """ Upload a security test on disk """
     with open(path, 'r') as source_code:
-        controller.upload_test(name=Path(path).name, code=source_code.read())
+        controller.upload_test(filename=Path(path).name, code=source_code.read())
         click.secho(f'Uploaded {path}', fg=Colors.GREEN.value)
 
 
