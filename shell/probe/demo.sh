fc3ea1ea8c749981174754c3f3f09f9c216841ef
david <coldduck@protonmail.com>
2023-01-04 09:42:31 -0500

    new draft of demo probe

--- shell/probe/demo.sh
+++ shell/probe/demo.sh
@@ -1,102 +1,104 @@
 #!/bin/bash
 
-PRELUDE_API="https://detect.prelude.org"
+PRELUDE_API="https://api.staging.preludsecurity.com"
 PRELUDE_TOKEN=$1
 
-confirm=true
 pause=3
 
 sys=$(uname -s)-$(uname -m)
 dos=$(echo $sys | tr '[:upper:]' '[:lower:]')
 
-while :
-do
+function get_test {
     temp=$(mktemp)
-    echo
-    echo "[*] Contacting Prelude for the next scheduled test"
     location=$(curl -sL -w %{url_effective} -o $temp -H "token:${PRELUDE_TOKEN}" -H "dos:${dos}" $PRELUDE_API)
     test=$(echo $location | grep -o '[0-9a-f]\{8\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{12\}')
-
     if [ -z "$test" ];then
-        echo "[✓] All scheduled tests are complete for today"
-        sleep $pause
-        read -p "[!] Would you like to install this script so it retests daily? (Y/N) " -n 1 -r
-        if [[ $REPLY =~ ^[Yy]$ ]];then
-            echo
-            echo "[*] Downloading installation script"
-            sleep $pause
-            temp=$(mktemp)
-            curl -sL -o $temp -H "token:${PRELUDE_TOKEN}" -H "dos:${dos}" $PRELUDE_API/download/install
-            chmod +x $temp
-            echo "[!] Installing software requires permission. You will be prompted for your password."
-            echo
-            echo "> The script is open source: https://github.com/preludeorg/libraries/blob/master/shell/install/install.sh"
-            echo "> You will need to provide your account ID and account token, which you can export from Build"
-            echo
-            read -p "Press ENTER to continue"
-            echo
-            read -p "Enter your account ID: " ACCOUNT_ID
-            read -p "Enter your account token: " ACCOUNT_TOKEN
-            echo $'\e[1;33m'
-            sudo $temp -a $ACCOUNT_ID -s $ACCOUNT_TOKEN
-            echo $'\e[0m'
-        else
-            echo
-            echo "[*] No big deal. You can always do this later."
-        fi
-        sleep $pause
-        echo
-        echo "See you back in the Prelude Platform!"
-        echo
-        break
+        echo -e "[!] No test found for the '${dos}' architecture"
+        exit 1
     else
-        echo -e "[✓] Retrieved test with identifier: ${test}"
-        sleep $pause
-        echo -e "[✓] Wrote test to temp file: ${temp}"
-        sleep $pause
-        ca=$(echo $location | sed -e 's|^[^/]*//||' -e 's|/.*$||')
-        if [ "$confirm" = true ];then
-            echo
-            echo -e "> Tests are served from the Prelude Central Authority: ${ca}"
-            echo -e "> This is one of several safety precautions built into test execution"
-            echo -e "> Read the full safety report: https://docs.prelude.org/docs/safety"
-            echo
-            read -p "Press ENTER to run the test"
-        fi
-        echo
-        echo "[*] Measuring the reaction from endpoint defense"
-        chmod +x $temp
-        sleep $pause
-
-        if test -f "$temp";then
-            echo "[!] Test was not quarantined. Moving on to execution."
-            sleep $pause
-            echo $'\e[1;33m'
-            $temp
-            res1=$?
-            $temp -cleanup
-            res2=$?
-            echo $'\e[0m'
-            max=$(( $res1 > $res2 ? $res1 : $res2 ))
-        else
-            max=9
-        fi
-
-        sleep $pause
-
-        dat=${test}:${max}
-        if [ "$confirm" = true ];then
-            echo "> Prelude collects the minimal amount of telemetry from each test"
-            echo "> Only the test identifier and result code are sent off the endpoint"
-            echo "> Results will display inside your Prelude Platform"
-            echo
-            sleep $pause
-            echo -e "[!] This test will send the following result: ${dat}"
-            echo
-            read -p "Press ENTER to continue"
-        fi
-        curl -sL -H "token:${PRELUDE_TOKEN}" -H "dos:${dos}" -H "dat:${dat}" $PRELUDE_API
-        sleep $pause
-        confirm=false
+        echo -e "[✓] Downloaded test to temporary file: ${temp}"
     fi
-done
+}
+
+function execute_test {
+    $temp
+    test_result=$?
+    echo "[✓] Test is complete"
+}
+
+function execute_cleanup {
+    $temp -cleanup
+    cleanup_result=$?
+    echo "[✓] Clean up is complete"
+}
+
+function post_results {
+    dat=${test}:${test_result}
+    curl -sL -H "token:${PRELUDE_TOKEN}" -H "dos:${dos}" -H "dat:${dat}" $PRELUDE_API
+    echo "[✓] Results have been saved to your account"
+}
+
+function install_probe {
+    echo "[+] Downloading installation script"
+    temp=$(mktemp)
+    curl -sL -o $temp -H "token:${PRELUDE_TOKEN}" -H "dos:${dos}" $PRELUDE_API/download/install
+    chmod +x $temp
+    read -p "Enter your Prelude Account ID: " ACCOUNT_ID
+    read -p "Enter your Prelude Account token: " ACCOUNT_TOKEN
+    sudo $temp -a $ACCOUNT_ID -s $ACCOUNT_TOKEN
+}
+
+echo
+echo "###########################################################################################################"
+echo
+echo "Rule #1: Will your computer quarantine a malicious Office document?"
+echo 
+echo "[*] There have been 1,501 CVE numbers tied to malicious Office documents"
+echo "[*] MITRE ATT&CK classifies a malicious Office macro as T1015"
+echo "[*] A malicious macro was the root of the recent Blah ransomware attack"
+echo
+echo "###########################################################################################################"
+echo
+echo "Starting test at: $(date +"%T")"
+echo
+echo "-----------------------------------------------------------------------------------------------------------"
+echo "[1] Downloading test..."
+echo
+get_test
+echo "-----------------------------------------------------------------------------------------------------------"
+echo "[2] Executing test..."
+echo
+execute_test
+echo "-----------------------------------------------------------------------------------------------------------"
+echo "[3] Running cleanup..."
+echo
+execute_cleanup
+echo "-----------------------------------------------------------------------------------------------------------"
+echo "[4] Recording results..."
+echo
+post_results
+echo "-----------------------------------------------------------------------------------------------------------"
+read -p "[Optional] Would you like to install this test so it runs daily? (y/N) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]];then
+    install_probe
+fi
+echo 
+echo 
+echo
+echo "###########################################################################################################"
+echo
+echo "Summary"
+echo 
+if [ "$test_result" = 100 ];then
+    echo "[✓] Your computer detected and responded to a malicious Office document dropped on the disk."
+else
+    echo "[!] Your computer was unable to detect a malicious Office document dropped on the disk."
+    echo "[!] This puts the machine at risk from a variety of computer attacks, including many tied to CVE exploits such as blah."
+    echo "[!] We advise you reach out to support@prelude.org for help selecting an appropriate endpoint defense."
+fi
+echo
+echo "[*] Learn about more tests you can run: https://docs.prelude.org/docs/rules"
+echo
+echo "###########################################################################################################"
+echo
