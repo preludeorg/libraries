33a0d55194b174ecd3cb9484b2814a72c7664759
Mahina Kaholokula <mahina.kaholokula@prelude.org>
2023-01-10 04:59:23 -0800

    update bash demo & add powershell (#183)
    * update bash demo & add powershell
    
    * add powershell
    
    * updates
    
    * updates

---
+++ shell/probe/demo.ps1
@@ -0,0 +1,150 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true, HelpMessage="Prelude Token")]
+    [String]$preludeToken
+)
+
+$PRELUDE_API="https://api.preludesecurity.com"
+$TEST_ID="b74ad239-2ddd-4b1e-b608-8397a43c7c54"
+
+if ($Env:PROCESSOR_ARCHITECTURE -eq 'AMD64') {
+    $arch = 'x86_64'
+} else {
+    $arch = 'arm64'
+}
+$dos = "windows-" + $arch
+
+$TempFile = [System.IO.Path]::GetTempFileName() | Rename-Item -NewName { [System.IO.Path]::ChangeExtension($_, "exe") } -PassThru
+
+$Headers = @{
+    'token' = $preludeToken
+    'dos' = $dos
+    'id' = $TEST_ID
+}
+
+function CheckRelevance {
+    Write-Host -ForegroundColor Green "`r`n[✓] Result: Success - server or workstation detected"
+}
+
+function DownloadTest {
+    try {
+        Invoke-WebRequest -URI $PRELUDE_API -Headers $Headers -MaximumRedirection 1 -OutFile $TempFile -PassThru | Out-Null
+    } catch {
+        $StatusCode = $_.Exception.Response.StatusCode.value__
+        Write-Host -ForegroundColor Red "`r`n[!] Failed to download test. Http response code: $StatusCode"
+        Exit 1
+    }
+    Write-Host -ForegroundColor Green "`r`n[✓] Wrote to temporary file: $TempFile"
+}
+
+function ExecuteTest {
+    & $TempFile
+    Write-Host -ForegroundColor Green "`r`n[✓] Test is complete"
+    return $LASTEXITCODE
+}
+
+function ExecuteCleanup {
+    & $TempFile clean
+    Write-Host -ForegroundColor Green "`r`n[✓] Clean up is complete"
+    return $LASTEXITCODE
+}
+
+function PostResults {
+    param([string]$testresult)
+    $Headers.dat = $TEST_ID + ":" + $testresult
+    Invoke-WebRequest -URI $PRELUDE_API -Headers $Headers -MaximumRedirection 1 | Out-Null
+}
+
+function InstallProbe {
+    Write-Host "`r`n[+] Downloading installation script"
+    $temp = [System.IO.Path]::GetTempFileName() | Rename-Item -NewName { [System.IO.Path]::ChangeExtension($_, "ps1") } -PassThru
+
+    try {
+        Invoke-WebRequest -URI $PRELUDE_API/download/install -Headers $Headers -MaximumRedirection 1 -OutFile $temp -PassThru | Out-Null
+    } catch {
+        $StatusCode = $_.Exception.Response.StatusCode.value__
+        Write-Host -ForegroundColor Red "`r`n[!] Failed to download installation script. Http response code: $StatusCode"
+        Exit 1
+    }
+
+    $account_id = Read-Host -Prompt "Enter your Prelude Account ID"
+    $account_token = Read-Host -Prompt "Enter your Prelude Account token"
+    & $temp $account_id $account_token
+    Remove-Item $TempFile -Force
+}
+
+Write-Host "
+###########################################################################################################
+
+Welcome to Prelude!
+
+Rule: Malicious files should quarantine when written to disk
+Test: Will your computer quarantine a malicious Office document?
+
+Malicious files are used to gain entry and conduct cyberattacks against corporate systems through seemingly
+innocuous email attachments or direct downloads. For example - a malicious macro was used by the BlueNoroff
+group in a ransomware attack (Dec. 2022)
+
+[+] ATT&CK mappings: T1204.002
+
+###########################################################################################################
+"
+Read-Host -Prompt "Press ENTER to continue"
+
+Write-Host "Starting test at: $(Get-Date -UFormat %T)
+
+-----------------------------------------------------------------------------------------------------------
+[0] Conducting relevance test"
+Start-Sleep -Seconds 3
+CheckRelevance
+
+Write-Host "-----------------------------------------------------------------------------------------------------------
+[1] Downloading test"
+DownloadTest
+
+Write-Host "-----------------------------------------------------------------------------------------------------------
+[2] Executing test
+"
+Start-Sleep -Seconds 3
+$testresult = ExecuteTest
+
+Write-Host "-----------------------------------------------------------------------------------------------------------
+[3] Running cleanup
+"
+Start-Sleep -Seconds 3
+$cleanresult = ExecuteCleanup
+Remove-Item $TempFile -Force
+
+$Status = ($testresult, $cleanresult | Measure-Object -Maximum).Maximum
+PostResults $Status
+
+Write-Host "-----------------------------------------------------------------------------------------------------------"
+
+$msg = "[Optional] Would you like to install this test so it runs daily? (y/n)"
+do {
+    $response = Read-Host -Prompt $msg
+    if ($response -eq 'y') {
+        InstallProbe
+    }
+} until ($response -eq 'n' -or $response -eq 'y')
+
+Write-Host "
+###########################################################################################################
+
+###########################################################################################################
+"
+
+if ($Status -eq 100 ) {
+    Write-Host "[✓] Good job! Your computer detected and responded to a malicious Office document dropped on the disk" -ForegroundColor Green
+} else {
+    Write-Host "[!] This test was able to verify the existence of this vulnerability on your machine, as well as drop a malicious
+Office document on the disk. If you have security controls in place that you suspect should have protected your
+host, you can use the artifacts above to try to understand why your defenses failed in your logs." -ForegroundColor Red
+}
+
+Write-Host "
+[*] To view your results for this test and others, run additional tests, or learn about conducting continuous security
+tests across your infrastructure, return to platform.preludesecurity.com.
+
+###########################################################################################################
+"
--- shell/probe/demo.sh
+++ shell/probe/demo.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+GREEN='\033[1;32m'
+RED='\033[1;31m'
+NC='\033[0m' # No Color
+
 PRELUDE_API="https://api.preludesecurity.com"
 PRELUDE_TOKEN=$1
 
@@ -8,18 +12,18 @@ id="b74ad239-2ddd-4b1e-b608-8397a43c7c54"
 dos=$(echo $sys | tr '[:upper:]' '[:lower:]')
 
 function check_relevance {
-    echo "This test is relevant for any laptop or server."
+    echo -e "${GREEN}[✓] Result: Success - server or workstation detected${NC}"
 }
 
 function download_test {
     temp=$(mktemp)
-    location=$(curl -sL -w %{url_effective} -o $temp -H "token:${PRELUDE_TOKEN}" -H "dos:${dos}" -H "id:${id}" $PRELUDE_API)
-    test=$(echo $location | grep -o '[0-9a-f]\{8\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{12\}')
+    location=$(curl -sfSL -w %{url_effective} -o $temp -H "token:${PRELUDE_TOKEN}" -H "dos:${dos}" -H "id:${id}" $PRELUDE_API)
+    test=$(echo $location | grep -o '[0-9a-f]\{8\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{12\}' | head -n 1)
     if [ -z "$test" ];then
-        echo -e "[!] No test found for the '${dos}' architecture"
+        echo -e "${RED}[!] Failed to download test${NC}"
         exit 1
     else
-        echo -e "[✓] Wrote to temporary file: ${temp}"
+        echo -e "${GREEN}[✓] Wrote to temporary file: ${temp}${NC}"
         chmod +x $temp
     fi
 }
@@ -28,27 +32,32 @@ function execute_test {
     $temp
     test_result=$?
     echo
-    echo "[✓] Test is complete"
+    echo -e "${GREEN}[✓] Test is complete${NC}"
 }
 
 function execute_cleanup {
     $temp -cleanup
     cleanup_result=$?
     echo
-    echo "[✓] Clean up is complete"
+    echo -e "${GREEN}[✓] Clean up is complete${NC}"
 }
 
 function post_results {
-    dat=${test}:${test_result}
-    curl -sL -H "token:${PRELUDE_TOKEN}" -H "dos:${dos}" -H "dat:${dat}" $PRELUDE_API
-    echo "[✓] Test result submitted"
+    max=$(( $test_result > $cleanup_result ? $test_result : $cleanup_result ))
+    dat=${test}:${max}
+    curl -sfSL -H "token:${PRELUDE_TOKEN}" -H "dos:${dos}" -H "dat:${dat}" $PRELUDE_API
 }
 
 function install_probe {
     echo "[+] Downloading installation script"
     temp=$(mktemp)
-    curl -sL -o $temp -H "token:${PRELUDE_TOKEN}" -H "dos:${dos}" $PRELUDE_API/download/install
+    curl -sfSL -o $temp -H "token:${PRELUDE_TOKEN}" -H "dos:${dos}" $PRELUDE_API/download/install
+    test -s "$temp" || {
+        echo -e "${RED}[!] Failed to download installation script${NC}"
+        exit 1
+    }
     chmod +x $temp
+    echo
     read -p "Enter your Prelude Account ID: " ACCOUNT_ID
     read -p "Enter your Prelude Account token: " ACCOUNT_TOKEN
     sudo $temp -a $ACCOUNT_ID -s $ACCOUNT_TOKEN
@@ -57,11 +66,16 @@ function install_probe {
 echo
 echo "###########################################################################################################"
 echo
-echo "Will your computer quarantine a malicious Office document?"
+echo "Welcome to Prelude!"
 echo
-echo "[*] There have been 1,798 CVE numbers tied to malicious Office documents"
-echo "[*] MITRE ATT&CK classifies a malicious Office macro as technique T1204.002"
-echo "[*] A malicious macro was used by the BlueNoroff group in a ransomware attack (Dec. 2022)"
+echo "Rule: Malicious files should quarantine when written to disk"
+echo "Test: Will your computer quarantine a malicious Office document?"
+echo
+echo "Malicious files are used to gain entry and conduct cyberattacks against corporate systems through seemingly"
+echo "innocuous email attachments or direct downloads. For example - a malicious macro was used by the BlueNoroff"
+echo "group in a ransomware attack (Dec. 2022)"
+echo
+echo "[+] ATT&CK mappings: T1204.002"
 echo
 echo "###########################################################################################################"
 echo
@@ -70,8 +84,7 @@ echo
 echo "Starting test at: $(date +"%T")"
 echo
 echo "-----------------------------------------------------------------------------------------------------------"
-echo "[0] Checking relevance"
-echo 
+echo "[0] Conducting relevance test"
 echo && sleep 3
 check_relevance
 echo "-----------------------------------------------------------------------------------------------------------"
@@ -86,29 +99,31 @@ echo "--------------------------------------------------------------------------
 echo "[3] Running cleanup"
 echo && sleep 3
 execute_cleanup
-echo "-----------------------------------------------------------------------------------------------------------"
-echo "[4] Saving results"
-echo && sleep 3
 post_results
 echo "-----------------------------------------------------------------------------------------------------------"
 read -p "[Optional] Would you like to install this test so it runs daily? (y/N) " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]];then
+    echo
     install_probe
 fi
 echo 
-echo
+echo "###########################################################################################################"
 echo
 echo "###########################################################################################################"
 echo
 if [ "$test_result" = 100 ];then
-    echo "[✓] Good job! Your computer detected and responded to a malicious Office document dropped on the disk."
+    echo -e "${GREEN}[✓] Good job! Your computer detected and responded to a malicious Office document dropped on "
+    echo -e "the disk${NC}"
 else
-    echo "[!] Your computer was unable to detect a malicious Office document dropped on the disk."
-    echo "[!] Reach out to support@prelude.org for help selecting an appropriate endpoint defense."
+    echo -e "${RED}[!] This test was able to verify the existence of this vulnerability on your machine, as well as drop"
+    echo "a malicious Office document on the disk. If you have security controls in place that you suspect should"
+    echo "have protected your host, you can use the artifacts above to try to understand why your defenses failed"
+    echo -e "in your logs${NC}"
 fi
 echo
-echo "[*] Learn about more tests you can run: https://docs.prelude.org/docs/rules"
+echo "[*] To view your results for this test and others, run additional tests, or learn about conducting continuous"
+echo "security tests across your infrastructure, return to platform.preludesecurity.com"
 echo
 echo "###########################################################################################################"
 echo
