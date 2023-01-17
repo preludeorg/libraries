4b08fc12034b3dd39c24dadcecd5531a4123ebe3
david <coldduck@protonmail.com>
2023-01-17 06:40:48 -0500

    swap filename for ID

--- python/cli/prelude_cli/views/build.py
+++ python/cli/prelude_cli/views/build.py
@@ -52,7 +52,7 @@ def create_test(controller, rule):
 
     controller.create_test(test_id=test_id, rule=rule)
     template = pkg_resources.read_text(templates, 'template.go')
-    template = template.replace('$FILENAME', basename)
+    template = template.replace('$ID', test_id)
     template = template.replace('$RULE', rule)
     template = template.replace('$CREATED', str(datetime.now()))
     controller.upload_test(filename=basename, code=template)
