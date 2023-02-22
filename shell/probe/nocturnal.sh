09b7d911b78711fedc6697805e6661d8bdbf7c89
Francis Mireault <mireaulf@gmail.com>
2023-02-22 15:03:57 -0500

    Single rm

--- shell/probe/nocturnal.sh
+++ shell/probe/nocturnal.sh
@@ -11,9 +11,9 @@ do
     location=$(curl -sL -w %{url_effective} -o $temp -H "token:${PRELUDE_TOKEN}" -H "dos:${dos}" -H "dat:${dat}" $PRELUDE_API)
     test=$(echo $location | grep -o '[0-9a-f]\{8\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{12\}' | head -n 1)
 
+    ts = 0
     if [ -z "$test" ];then
-        rm -f $temp
-        sleep 14400
+        ts = 14400
     else
         ca=$(echo $location | sed -e 's|^[^/]*//||' -e 's|/.*$||')
         if [ -z "$PRELUDE_CA" ] || [ "$PRELUDE_CA" == "$ca" ];then
@@ -27,6 +27,7 @@ do
                 dat="${test}:9"
             fi
         fi
-        rm -f $temp
     fi
+    rm -f $temp
+    sleep $ts
 done
