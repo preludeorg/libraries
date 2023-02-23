35f6e562977a8c108cca0bb95d91fc95838750b1
makk94 <mahina.kaholokula@prelude.org>
2023-02-23 15:09:22 -0800

    revert

--- shell/probe/nocturnal.sh
+++ shell/probe/nocturnal.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 
 PRELUDE_API=${PRELUDE_API:="https://api.preludesecurity.com"}
-TEMPDIR=${TMPDIR:="/tmp"}
 
 sys=$(uname -s)-$(uname -m)
 dos=$(echo $sys | tr '[:upper:]' '[:lower:]')
 
 while :
 do
-    redirect=$(curl -sfS -w %{redirect_url} -H "token:${PRELUDE_TOKEN}" -H "dos:${dos}" -H "dat:${dat}" $PRELUDE_API)
-    test=$(echo $redirect | grep -o '[0-9a-f]\{8\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{12\}' | head -n 1)
-    if [ ! -z "$test" ];then
-        temp="${TEMPDIR}/detect"
-        resp=$(curl -sfS -o $temp $redirect)
-        if [ -f $temp ];then
-            ca=$(echo $location | sed -e 's|^[^/]*//||' -e 's|/.*$||')
-            if [ -z "$PRELUDE_CA" ] || [ "$PRELUDE_CA" == "$ca" ];then
-                chmod +x $temp
+    temp=$(mktemp)
+    echo "created ${temp}"
+    location=$(curl -sL -w %{url_effective} -o $temp -H "token:${PRELUDE_TOKEN}" -H "dos:${dos}" -H "dat:${dat}" $PRELUDE_API)
+    test=$(echo $location | grep -o '[0-9a-f]\{8\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{12\}' | head -n 1)
 
-                if test -f "$temp";then
-                    $temp
-                    dat="${test}:$?"
-                    $temp -cleanup
-                else
-                    dat="${test}:9"
-                fi
+    ts=0
+    if [ -z "$test" ];then
+        ts=14400
+    else
+        ca=$(echo $location | sed -e 's|^[^/]*//||' -e 's|/.*$||')
+        if [ -z "$PRELUDE_CA" ] || [ "$PRELUDE_CA" == "$ca" ];then
+            chmod +x $temp
+            echo "testing!"
+
+            if test -f "$temp";then
+                $temp
+                dat="${test}:$?"
+                $temp -cleanup
+            else
+                dat="${test}:9"
             fi
-
-            rm -f $temp
-            continue
         fi
     fi
-    sleep 14400
+    echo "removing temp"
+    rm -f $temp
+    echo "sleeping ${ts}"
+    sleep $ts
 done
