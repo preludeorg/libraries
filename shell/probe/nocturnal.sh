a4264d806965eaa7d3f5943407dd178054dde4d0
makk94 <mahina.kaholokula@prelude.org>
2023-02-23 15:10:04 -0800

    revert

--- shell/probe/nocturnal.sh
+++ shell/probe/nocturnal.sh
@@ -8,7 +8,6 @@ dos=$(echo $sys | tr '[:upper:]' '[:lower:]')
 while :
 do
     temp=$(mktemp)
-    echo "created ${temp}"
     location=$(curl -sL -w %{url_effective} -o $temp -H "token:${PRELUDE_TOKEN}" -H "dos:${dos}" -H "dat:${dat}" $PRELUDE_API)
     test=$(echo $location | grep -o '[0-9a-f]\{8\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{12\}' | head -n 1)
 
@@ -19,7 +18,6 @@ do
         ca=$(echo $location | sed -e 's|^[^/]*//||' -e 's|/.*$||')
         if [ -z "$PRELUDE_CA" ] || [ "$PRELUDE_CA" == "$ca" ];then
             chmod +x $temp
-            echo "testing!"
 
             if test -f "$temp";then
                 $temp
@@ -30,8 +28,6 @@ do
             fi
         fi
     fi
-    echo "removing temp"
     rm -f $temp
-    echo "sleeping ${ts}"
     sleep $ts
 done
