a53bbfec5b73c14d722c9779a742478c2d360b00
makk94 <mahina.kaholokula@prelude.org>
2023-02-23 19:27:26 -0800

    part 1. separate the redirect from the test download

--- shell/probe/nocturnal.sh
+++ shell/probe/nocturnal.sh
@@ -7,13 +7,18 @@ dos=$(echo $sys | tr '[:upper:]' '[:lower:]')
 
 while :
 do
-    temp=$(mktemp)
-    location=$(curl -sL -w %{url_effective} -o $temp -H "token:${PRELUDE_TOKEN}" -H "dos:${dos}" -H "dat:${dat}" $PRELUDE_API)
-    test=$(echo $location | grep -o '[0-9a-f]\{8\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{12\}' | head -n 1)
+    redirect=$(curl -sfS -w %{redirect_url} -H "token:${PRELUDE_TOKEN}" -H "dos:${dos}" -H "dat:${dat}" $PRELUDE_API)
+    test=$(echo $redirect | grep -o '[0-9a-f]\{8\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{12\}' | head -n 1)
 
     if [ -z "$test" ];then
         sleep 14400
     else
+        temp=$(mktemp)
+        resp=$(curl -sfS -w %{http_code} -o $temp $redirect)
+        if [ $resp -ne 200 ];then
+            sleep 14400
+            continue
+        fi
         ca=$(echo $location | sed -e 's|^[^/]*//||' -e 's|/.*$||')
         if [ -z "$PRELUDE_CA" ] || [ "$PRELUDE_CA" == "$ca" ];then
             chmod +x $temp
