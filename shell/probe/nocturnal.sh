0bcd6844741151ea4bd8ccaf68f967b7c104964b
privateducky <coldduck@protonmail.com>
2023-03-07 15:46:57 -0500

    cleaner

--- shell/probe/nocturnal.sh
+++ shell/probe/nocturnal.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 PRELUDE_API=${PRELUDE_API:="https://api.preludesecurity.com"}
-PRELUDE_DIR=${PRELUDE_DIR:="."}
+PRELUDE_DIR=${PRELUDE_DIR:="./cache"}
 PRELUDE_SLEEP=${PRELUDE_SLEEP:=14440}
 
 dos=$(echo $(uname -s)-$(uname -m))
 
 while :
 do  
-    exe=$PRELUDE_DIR/$(uuidgen).vst
-    location=$(curl -sL -w %{url_effective} -o $exe -H "token:${PRELUDE_TOKEN}" -H "dos:${dos}" -H "dat:${dat}" $PRELUDE_API)
+    exe=$PRELUDE_DIR/$(uuidgen)
+    location=$(curl -sL -w %{url_effective} --create-dirs -o $exe -H "token:${PRELUDE_TOKEN}" -H "dos:${dos}" -H "dat:${dat}" $PRELUDE_API)
     
     if [ -s $exe ];then
         test=$(echo $location | grep -o '[0-9a-f]\{8\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{12\}' | head -n 1)
@@ -23,7 +23,7 @@ do
             dat="${test}:$?"
         fi
     else
-        find $PRELUDE_DIR -type f -name "*.vst" -delete
+        find $PRELUDE_DIR -type f -name "*" -mmin -1 -delete
         sleep $PRELUDE_SLEEP
     fi
 done
