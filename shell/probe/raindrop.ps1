4f20e74edecebb423db765689459a3c12fcbe964
privateducky <coldduck@protonmail.com>
2023-03-08 08:27:37 -0500

    ensuring raindrop is aligned w/nocturnal changes

--- shell/probe/raindrop.ps1
+++ shell/probe/raindrop.ps1
@@ -1,62 +1,63 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$Sleep = FromEnv "PRELUDE_SLEEP" 14440
+$Dir = FromEnv "PRELUDE_DIR" ".vst"
+$CA = FromEnv "PRELUDE_CA" "prelude-account-prod-us-west-1.s3.amazonaws.com"
+
+$Api = "https://api.preludesecurity.com"
+$Dos = "windows-" + $Env:PROCESSOR_ARCHITECTURE
+
 function Run {
     Param([String]$Dat = "")
 
-    $TempFile = [System.IO.Path]::GetTempFileName() | Rename-Item -NewName { [System.IO.Path]::ChangeExtension($_, "exe") } -PassThru
-
-    $Headers = @{
-        'token' = $Token
-        'dos' = $Dos
-        'dat' = $Dat
-    }
     try {
-        $Response = Invoke-WebRequest -URI $Address -UseBasicParsing -Headers $Headers -MaximumRedirection 1 -OutFile $TempFile -PassThru
+        $Vst = New-Item -Path "$Dir\$(New-Guid).exe" -Force
+        $Headers = @{
+            'token' = FromEnv "PRELUDE_TOKEN"
+            'dos' = $Dos
+            'dat' = $Dat
+        }
+        $Response = Invoke-WebRequest -URI $Api -UseBasicParsing -Headers $Headers -MaximumRedirection 1 -OutFile $Vst -PassThru
+        
+        if ($CA -ne $Response.BaseResponse.ResponseUri.Authority) {
+            Write-Output "[P] Bad authority: $Response.BaseResponse.ResponseUri.Authority"
+            exit 0
+        }
+        
+        $Test = $Response.BaseResponse.ResponseUri.AbsolutePath.Split("/")[-1].Split("_")[0]
+        if (-not $Test) {
+            return
+        }
+
+        Write-Output "[P] Running $Test [$Vst]" 
+        $Code = Execute $Vst   
+        Run -Dat "${Test}:$Code"
     } catch {
-        $StatusCode = $_.Exception.Response.StatusCode.value__
-        Write-Output "ERROR: Failed to reach Prelude Service. " + $StatusCode
         return
     }
-    if ($CA -and $CA -ne $Response.BaseResponse.ResponseUri.Authority) {
-        return
-    }
-    $Test = $Response.BaseResponse.ResponseUri.AbsolutePath.Split("/")[-1].Split("_")[0]
-    if (-not $Test) {
-        Write-Output "INFO: Done running tests"
-        return
-    }
-
-    $TestExit = Execute $TempFile
-    Start-Process -FilePath $TempFile -ArgumentList "clean" -Wait -NoNewWindow -PassThru
-
-    Remove-Item $TempFile -Force
-    Run -Dat $($Test + ":" + $TestExit)
 }
 
 function Execute { 
     Param([String]$File)
+
     try {
-        return (Start-Process -FilePath $File -Wait -NoNewWindow -PassThru).ExitCode
+        $Code = (Start-Process -FilePath $File -Wait -NoNewWindow -PassThru).ExitCode
+        return If (Test-Path $Vst) {$Code} Else {127}
     } catch [System.UnauthorizedAccessException] {
         return 126
     } catch [System.InvalidOperationException] {
         return 127
     } catch {
-        Write-Host $_
         return 1
     }
 }
 
 function FromEnv { param ([string]$envVar, [string]$default)
-    $envVal = [Environment]::GetEnvironmentVariable($envVar, "User")
+    $envVal = [Environment]::GetEnvironmentVariable($envVar, "#User")
     if ($envVal) { return $envVal } else { return $default }
 }
 
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$Address = FromEnv "PRELUDE_API" "https://api.preludesecurity.com"
-$Token = FromEnv "PRELUDE_TOKEN" ""
-$CA = FromEnv "PRELUDE_CA" ""
-$Dos = "windows-" + $Env:PROCESSOR_ARCHITECTURE
-
 while ($true) {
     Run
-    Start-Sleep -Seconds 14400
+    Start-Sleep -Seconds $Sleep
 }
