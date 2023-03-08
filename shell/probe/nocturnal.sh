70c9684f1af352748f2cf96a2782c010f531cc16
david <49954156+privateducky@users.noreply.github.com>
2023-03-08 07:33:31 -0500

    Shell probes no longer use temp only files (PRELUDE_DIR) (#427)
    * revising shell probe logic
    
    * cleaner
    
    * echo
    
    * echo
    
    * echo
    
    * echo
    
    * echo
    
    * echo
    
    * better
    
    * reads
    
    * reads
    
    * reads
    
    * reads
    
    * reads
    
    * reads
    
    * reads
    
    * reads
    
    * reads
    
    * tweak
    
    * tweak
    
    * tweak

--- shell/probe/nocturnal.sh
+++ shell/probe/nocturnal.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
-PRELUDE_API=${PRELUDE_API:="https://api.preludesecurity.com"}
+PRELUDE_SLEEP=${PRELUDE_SLEEP:=14440}
+PRELUDE_DIR=${PRELUDE_DIR:=".vst"}
+PRELUDE_CA=${PRELUDE_CA:="prelude-account-prod-us-west-1.s3.amazonaws.com"}
 
-sys=$(uname -s)-$(uname -m)
-dos=$(echo $sys | tr '[:upper:]' '[:lower:]')
+api="https://api.preludesecurity.com"
+dos=$(uname -s)-$(uname -m)
 
 while :
-do
-    temp=$(mktemp)
-    location=$(curl -sL -w %{url_effective} -o $temp -H "token:${PRELUDE_TOKEN}" -H "dos:${dos}" -H "dat:${dat}" $PRELUDE_API)
+do  
+    exe=$PRELUDE_DIR/$(uuidgen)
+    location=$(curl -sL -w %{url_effective} --create-dirs -o $exe -H "token:${PRELUDE_TOKEN}" -H "dos:${dos}" -H "dat:${dat}" $api)
     test=$(echo $location | grep -o '[0-9a-f]\{8\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{12\}' | head -n 1)
 
-    if [ -z "$test" ];then
-        sleep 14400
-    else
+    if [ $test ];then
         ca=$(echo $location | sed -e 's|^[^/]*//||' -e 's|/.*$||')
-        if [ -z "$PRELUDE_CA" ] || [ "$PRELUDE_CA" == "$ca" ];then
-            chmod +x $temp
 
-            if test -f "$temp";then
-                $temp
-                dat="${test}:$?"
-                $temp -cleanup
-            else
-                dat="${test}:9"
-            fi
+        if [ "$PRELUDE_CA" == "$ca" ];then
+            echo "[P] Running $test as $exe"
+            chmod +x $exe && $exe
+            code=$?
+            dat="${test}:$([[ -f $exe ]] && echo $code || echo 127)"
+        else
+            echo "[P] Bad authority: $ca" && exit 0
         fi
+    else
+        find $PRELUDE_DIR -type f -name "*" -mmin -5 -delete
+        unset dat && sleep $PRELUDE_SLEEP
     fi
 done
