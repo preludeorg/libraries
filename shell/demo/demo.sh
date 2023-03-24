dd9f0207ad837f8bbd217a7eed633cb2e3c6df9d
makk94 <mahina.kaholokula@prelude.org>
2023-03-24 10:02:17 -0700

    nit

--- shell/demo/demo.sh
+++ shell/demo/demo.sh
@@ -38,11 +38,10 @@ function download_test {
     local _test_id=$1
     local _temp=$2
     echo -e "\n[ ] Downloading test"
-    sleep 1
 
     location=$(curl -sL -w %{url_effective} -o $_temp -H "token:${PRELUDE_TOKEN}" -H "dos:${dos}" -H "id:${_test_id}" $PRELUDE_API)
     test=$(echo $location | grep -o '[0-9a-f]\{8\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{12\}' | head -n 1)
-    tput cuu 1 && tput el
+    sleep 1 && tput cuu 1 && tput el
     if [ -z "$test" ];then
         echo -e "${RED}[!] Failed to download test${NC}"
         exit 1
