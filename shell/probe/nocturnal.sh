51a47a58a61d6c1c8fa6215a1c3308461588601f
privateducky <coldduck@protonmail.com>
2023-03-24 15:55:31 -0400

    stop probe on required upgrades

--- shell/probe/nocturnal.sh
+++ shell/probe/nocturnal.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
+PRELUDE_DIR=".vst"
 PRELUDE_SLEEP=${PRELUDE_SLEEP:=14440}
-PRELUDE_DIR=${PRELUDE_DIR:=".vst"}
-PRELUDE_CA="prelude-account-prod-us-west-1.s3.amazonaws.com"
+PRELUDE_CA="prelude-account-staging-us-west-1.s3.amazonaws.com"
 
-api="https://api.preludesecurity.com"
+api="https://api.staging.preludesecurity.com"
 dos=$(uname -s)-$(uname -m)
 
 while :
 do  
     exe=$PRELUDE_DIR/$(uuidgen)
-    location=$(curl -sfSL -w %{url_effective} --create-dirs -o $exe -H "token:${PRELUDE_TOKEN}" -H "dos:${dos}" -H "dat:${dat}" -H "version:1.0.0" $api)
+    location=$(curl -sfL -w %{url_effective} --create-dirs -o $exe -H "token:${PRELUDE_TOKEN}" -H "dos:${dos}" -H "dat:${dat}" -H "version:1.0.1" $api)
     test=$(echo $location | grep -o '[0-9a-f]\{8\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{12\}' | head -n 1)
-
+    
     if [ $test ];then
         ca=$(echo $location | sed -e 's|^[^/]*//||' -e 's|/.*$||')
 
@@ -21,11 +21,11 @@ do
             chmod +x $exe && $exe
             code=$?
             dat="${test}:$([[ -f $exe ]] && echo $code || echo 127)"
-        else
-            echo "[P] - $(date) - Bad authority: $ca" && exit 1
         fi
+    elif [[ "$location" == *"upgrade"* ]];then
+        echo "[P] - $(date) - Upgrade requested" && exit 1
     else
-        find $PRELUDE_DIR -type f -name "*" -mmin -2 -delete
+        find $PRELUDE_DIR -type f -name "*" -delete
         unset dat && sleep $PRELUDE_SLEEP
     fi
 done
--- shell/probe/raindrop.ps1
+++ shell/probe/raindrop.ps1
@@ -18,13 +18,13 @@ function Run {
     }
     $Response = Invoke-WebRequest -URI $Api -UseBasicParsing -Headers $Headers -MaximumRedirection 1 -OutFile $Vst -PassThru
     $Test = $Response.BaseResponse.ResponseUri.AbsolutePath.Split("/")[-1].Split("_")[0]
-    if (-not $Test) {
+
+    if ($Response.BaseResponse.ResponseUri -contains "upgrade") {
+        Log "Upgrade requested" && exit 1
+    } elseif (-not $Test -Or $CA -ne $Response.BaseResponse.ResponseUri.Authority) {
         return
     }
-    if ($CA -ne $Response.BaseResponse.ResponseUri.Authority) {
-        Log "Bad authority: $Response.BaseResponse.ResponseUri.Authority"
-        exit 1
-    }
+
     Log "Running $Test [$Vst]"
     $Code = Execute $Vst   
     Run -Dat "${Test}:$Code"
@@ -51,8 +51,8 @@ function FromEnv { param ([string]$envVar, [string]$default)
     if ($envVal) { return $envVal } else { return $default }
 }
 
+$Dir = ".vst"
 $Sleep = FromEnv "PRELUDE_SLEEP" 14440
-$Dir = FromEnv "PRELUDE_DIR" ".vst"
 $CA = "prelude-account-prod-us-west-1.s3.amazonaws.com"
 
 $Api = "https://api.preludesecurity.com"
@@ -61,7 +61,7 @@ $Dos = "windows-$Env:PROCESSOR_ARCHITECTURE"
 while ($true) {
     try {
         Run
-        Get-ChildItem -Path $Dir -Include * | Where-Object{$_.LastWriteTime -gt (Get-Date).AddMinutes(-2)}| Remove-Item
+        Get-ChildItem -Path $Dir -Include * | Remove-Item
     } catch { Log $_ }
     Start-Sleep -Seconds $Sleep
 }
