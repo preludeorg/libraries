8adb3a1521c241ab79fcf4ea73d1d3e550d98314
david <49954156+privateducky@users.noreply.github.com>
2023-03-24 07:19:56 -0400

    Update nocturnal.sh

--- shell/probe/nocturnal.sh
+++ shell/probe/nocturnal.sh
@@ -5,13 +5,12 @@ PRELUDE_DIR=${PRELUDE_DIR:=".vst"}
 PRELUDE_CA=${PRELUDE_CA:="prelude-account-prod-us-west-1.s3.amazonaws.com"}
 
 api="https://api.preludesecurity.com"
-version="1.0.0"
 dos=$(uname -s)-$(uname -m)
 
 while :
 do  
     exe=$PRELUDE_DIR/$(uuidgen)
-    location=$(curl -sfSL -w %{url_effective} --create-dirs -o $exe -H "token:${PRELUDE_TOKEN}" -H "dos:${dos}" -H "dat:${dat}" -H "version:${version}" $api)
+    location=$(curl -sfSL -w %{url_effective} --create-dirs -o $exe -H "token:${PRELUDE_TOKEN}" -H "dos:${dos}" -H "dat:${dat}" -H "version:1.0.0" $api)
     test=$(echo $location | grep -o '[0-9a-f]\{8\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{12\}' | head -n 1)
 
     if [ $test ];then
