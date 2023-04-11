689965d32800a8456e9ff4638ad1687db5b14fa8
Alex <43142871+khyberspache@users.noreply.github.com>
2023-04-11 10:21:11 -0400

    renaming things

--- python/cli/prelude_cli/views/partner.py
+++ python/cli/prelude_cli/views/partner.py
@@ -14,23 +14,23 @@ def partner(ctx):
 
 
 @partner.command('endpoints')
-@click.option('--control_name', required=True, help='control name (e.g. "CrowdStrike")')
+@click.option('--partner_name', required=True, help='control name (e.g. "CrowdStrike")')
 @click.option('--platform', required=True, help='platform name (e.g. "windows")', type=click.Choice(['windows', 'linux', 'darwin'], case_sensitive=False))
 @click.option('--hostname', default='', help='hostname pattern (e.g. "mycompany-c24oi444")')
 @click.option('--offset', default=0, help='API pagination offset', type=int)
 @click.pass_obj
 @handle_api_error
-def partner_endpoints(controller, control_name, platform, hostname, offset):
+def partner_endpoints(controller, partner_name, platform, hostname, offset):
     """ Get a list of endpoints from all partners """
-    print_json(controller.partner_endpoints(control_name=control_name, platform=platform, hostname=hostname, offset=offset))
+    print_json(controller.partner_endpoints(partner_name=partner_name, platform=platform, hostname=hostname, offset=offset))
 
 
 @partner.command('deploy')
 @click.confirmation_option(prompt='Are you sure?')
-@click.option('--control_name', required=True, help='control name (e.g. "CrowdStrike")')
+@click.option('--partner_name', required=True, help='control name (e.g. "CrowdStrike")')
 @click.option('--host_ids', required=True, help='a list of host IDs to deploy to', type=list[str])
 @click.pass_obj
 @handle_api_error
-def detach_partner(controller, control_name, host_ids):
+def detach_partner(controller, partner_name, host_ids):
     """ Detach an existing partner from your account """
-    controller.partner_deploy(control_name=control_name, host_ids=host_ids)
+    controller.partner_deploy(partner_name=partner_name, host_ids=host_ids)
--- python/sdk/prelude_sdk/controllers/partner_controller.py
+++ python/sdk/prelude_sdk/controllers/partner_controller.py
@@ -10,12 +10,12 @@ class PartnerController:
         self.account = account
 
     @verify_credentials
-    def partner_endpoints(self, control_name: str, platform: str, hostname: str = '', offset: int = 0):
+    def partner_endpoints(self, partner_name: str, platform: str, hostname: str = '', offset: int = 0):
         """ Get a list of endpoints from all partners """
         with Spinner():
             params = dict(platform=platform, hostname=hostname, offset=offset)
             res = requests.get(
-                f'{self.account.hq}/partner/{control_name}',
+                f'{self.account.hq}/partner/{partner_name}',
                 headers=self.account.headers,
                 params=params,
                 timeout=30
@@ -25,12 +25,12 @@ class PartnerController:
             raise Exception(res.text)
 
     @verify_credentials
-    def partner_deploy(self, control_name: str, host_ids: list[str]):
+    def partner_deploy(self, partner_name: str, host_ids: list[str]):
         """ Deploy probes on all specified partner endpoints """
         with Spinner():
             params = dict(host_ids=host_ids)
             res = requests.post(
-                f'{self.account.hq}/partner/{control_name}',
+                f'{self.account.hq}/partner/{partner_name}',
                 headers=self.account.headers,
                 json=params,
                 timeout=30
