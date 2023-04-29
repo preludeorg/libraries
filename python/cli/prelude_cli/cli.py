99113e270442be6057ca70e08387e77c53dfe47a
privateducky <49954156+privateducky@users.noreply.github.com>
2023-04-29 13:05:40 -0400

    kill interactive mode (#567)

--- python/cli/prelude_cli/cli.py
+++ python/cli/prelude_cli/cli.py
@@ -6,21 +6,16 @@ from prelude_cli.views.detect import detect
 from prelude_cli.views.partner import partner
 from prelude_sdk.models.account import Account
 from prelude_cli.views.configure import configure
-from prelude_cli.views.interactive import interactive as interactive_command
 
 
 @click.group(invoke_without_command=True)
 @click.version_option()
 @click.pass_context
 @click.option('--profile', default='default', help='The prelude keychain profile to use', show_default=True)
-@click.option('--interactive', help='Open interactive wizard (cannot be used with a subcommand)', default=False, is_flag=True)
-def cli(ctx, profile, interactive):
+def cli(ctx, profile):
     ctx.obj = Account(profile=profile)
     if ctx.invoked_subcommand is None:
-        if interactive:
-            ctx.invoke(interactive_command)
-        else:
-            click.echo(ctx.get_help())
+        click.echo(ctx.get_help())
 
 
 cli.add_command(iam)
