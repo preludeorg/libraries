0c57a528e875dabd21dee44851172afb72020815
Alex <43142871+khyberspache@users.noreply.github.com>
2023-05-15 14:11:46 -0400

    bumpin up the version, should we change the defaults here too

--- shell/probe/raindrop.ps1
+++ shell/probe/raindrop.ps1
@@ -36,7 +36,7 @@ while ($true) {
             'token' = FromEnv "PRELUDE_TOKEN"
             'dos' = $Dos
             'dat' = $Dat
-            'version' = "1.0"
+            'version' = "1.1"
         }
         $Response = Invoke-WebRequest -URI $Api -UseBasicParsing -Headers $Headers -MaximumRedirection 1 -OutFile $Vst -PassThru
         $Test = $Response.BaseResponse.ResponseUri.AbsolutePath.Split("/")[-1].Split("_")[0]
