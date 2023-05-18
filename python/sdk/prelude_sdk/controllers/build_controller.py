5a8e3a9b45ce8a0a451b2f89027ded03194f8740
makk94 <mahina.kaholokula@prelude.org>
2023-05-18 09:20:00 -0700

    move Get and DownloadTest to /detect/ path

--- python/sdk/prelude_sdk/controllers/build_controller.py
+++ python/sdk/prelude_sdk/controllers/build_controller.py
@@ -35,30 +35,6 @@ class BuildController:
             raise Exception(res.text)
 
     @verify_credentials
-    def get_test(self, test_id):
-        """ Get properties of an existing test """
-        res = requests.get(
-            f'{self.account.hq}/build/tests/{test_id}',
-            headers=self.account.headers,
-            timeout=10
-        )
-        if res.status_code == 200:
-            return res.json()
-        raise Exception(res.text)
-
-    @verify_credentials
-    def download(self, test_id, filename):
-        """ Clone a test file or attachment"""
-        res = requests.get(
-            f'{self.account.hq}/build/tests/{test_id}/{filename}',
-            headers=self.account.headers,
-            timeout=10
-        )
-        if res.status_code == 200:
-            return res.content
-        raise Exception(res.text)
-
-    @verify_credentials
     def upload(self, test_id, filename, data, binary=False):
         """ Upload a test or attachment """
         h = self.account.headers | ({'Content-Type': 'application/octet-stream'} if binary else {})
--- python/sdk/tests/test_build_controller.py
+++ python/sdk/tests/test_build_controller.py
@@ -16,20 +16,12 @@ class TestBuildController:
         """Setup the test class"""
         self.test_id = str(uuid.uuid4())
         self.test_name = 'test'
-        self.test_unit = 'av'
         self.build = BuildController(pytest.account)
 
     def test_create_test(self, unwrap):
         """Test create_test method"""
-        unwrap(self.build.create_test)(self.build, test_id=self.test_id, name=self.test_name, unit=self.test_unit)
+        unwrap(self.build.create_test)(self.build, test_id=self.test_id, name=self.test_name, unit='bulletin')
         assert True
-
-    def test_get_test(self, unwrap):
-        """Test get_test method"""
-        res = unwrap(self.build.get_test)(self.build, test_id=self.test_id)
-        assert res['id'] == self.test_id
-        assert res['name'] == self.test_name
-        assert res['unit'] == self.test_unit
 
     def test_upload(self, unwrap):
         """Test upload method"""
@@ -40,15 +32,6 @@ class TestBuildController:
         unwrap(self.build.upload)(self.build, test_id=self.test_id, filename=f'{self.test_id}.go', data=template)
         res = unwrap(self.build.get_test)(self.build, test_id=self.test_id)
         assert f'{self.test_id}.go' in res['attachments']
-
-    def test_download(self, unwrap):
-        """Test download method"""
-        res = unwrap(self.build.download)(self.build, test_id=self.test_id, filename=f'{self.test_id}.go')
-        assert res is not None
-        with open(f'{self.test_id}.go', 'wb') as f:
-            f.write(res)
-        assert os.path.isfile(f'{self.test_id}.go')
-        os.remove(f'{self.test_id}.go')
 
     def test_delete_test(self, unwrap):
         """Test delete_test method"""
