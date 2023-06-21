3a5eb1be2275744aabb6afba5e01dd9c4662f6c7
Alex <43142871+khyberspache@users.noreply.github.com>
2023-06-21 11:59:38 -0400

    Shuffling code and names

--- shell/probe/vision.sh
+++ shell/probe/vision.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+dos=$(uname -s)-$(uname -m)
 mkdir -p /opt/preludesecurity
-curl -sL "https://api.preludesecurity.com/download/nocturnal" -H "dos:linux-amd64" > /opt/preludesecurity/nocturnal
+curl -sL "https://api.preludesecurity.com/download/nocturnal" -H "dos:${dos}" > /opt/preludesecurity/nocturnal
 chmod +x /opt/preludesecurity/nocturnal
 /opt/preludesecurity/nocturnal > /opt/preludesecurity/detect.log 2>&1 &
 
