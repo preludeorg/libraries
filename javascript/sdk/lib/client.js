2917bba67dfcd0be1a50577c6531ae4dad235df7
stephanwampouille <stephan@preludesecurity.org>
2023-07-07 14:24:03 -0400

    WIP remove typescript from JS SDK

---
+++ javascript/sdk/lib/client.js
@@ -0,0 +1,197 @@
+/**
+ * Asserts that the provided URL is valid.
+ *
+ * @param {URL} url - The URL to validate.
+ */
+function assertValidURL(url) {
+  const validScheme = url.protocol === "http:" || url.protocol === "https:";
+
+  if (!validScheme) {
+    throw new Error(`Invalid URL ${url.toString()}`);
+  }
+}
+
+/**
+ * Default headers used in HTTP requests.
+ *
+ * @type {HeadersInit}
+ */
+const defaultHeaders = {
+  "Content-Type": "application/json",
+  _product: "js-sdk",
+};
+
+/**
+ * Combines multiple sets of headers into a single headers object.
+ *
+ * @param {HeadersInit[]} headers - The headers to combine.
+ * @returns {HeadersInit} The combined headers.
+ */
+function combineHeaders(...headers) {
+  return headers.reduce((acc, h) => ({ ...acc, ...h }), {});
+}
+
+/**
+ * Custom error class for API errors.
+ *
+ * @class
+ * @extends Error
+ */
+class APIError extends Error {
+  /**
+   * Constructs a new instance of the APIError class.
+   *
+   * @constructor
+   * @param {string} message - The error message.
+   * @param {number} status - The HTTP status code associated with the error.
+   */
+  constructor(message, status) {
+    super(message);
+    this.name = "APIError";
+    this.status = status;
+  }
+}
+
+/**
+ * Client class for making HTTP requests.
+ *
+ * @class
+ * @default
+ */
+export default class Client {
+  /**
+   * Create a new instance of the Client class.
+   *
+   * @constructor
+   * @param {string} host - The host URL.
+   * @param {Credentials} [credentials] - The authentication credentials.
+   * @param {RequestInterceptor} [requestInterceptor] - The request interceptor function.
+   */
+  constructor(host, credentials, requestInterceptor) {
+    /**
+     * The host URL.
+     *
+     * @type {URL}
+     * @private
+     */
+    this.#host = new URL(host);
+    assertValidURL(this.#host);
+
+    /**
+     * The authentication credentials.
+     *
+     * @type {Credentials}
+     * @private
+     */
+    this.#credentials = credentials;
+
+    /**
+     * The request interceptor function.
+     *
+     * @type {RequestInterceptor}
+     * @private
+     */
+    this.#requestInterceptor = requestInterceptor;
+  }
+
+  /**
+   * Sets the credentials for the API client.
+   *
+   * @param {Credentials} credentials - The authentication credentials to set.
+   */
+  setCredentials(credentials) {
+    this.#credentials = credentials;
+  }
+
+  /**
+   * Ensures that the credentials are properly set and returns them.
+   *
+   * @private
+   * @returns {Credentials} The validated credentials.
+   * @throws {Error} If the account ID or token is not set.
+   */
+  #ensureCredentials() {
+    if (typeof this.#credentials?.account !== "string") {
+      throw new Error("Account ID not set");
+    }
+
+    if (typeof this.#credentials?.token !== "string") {
+      throw new Error("Token not set");
+    }
+
+    return this.#credentials;
+  }
+
+  /**
+   * Fetches the specified request.
+   *
+   * @private
+   * @async
+   * @param {Request} request - The request object to fetch.
+   * @param {RequestInit} [options={}] - Optional request options.
+   * @returns {Promise<Response>} A promise that resolves to the response.
+   * @throws {APIError}
+   */
+  async #fetch(request, options = {}) {
+    const response = await fetch(request);
+
+    if (options.redirect === "manual" && response.status === 302) {
+      return response;
+    }
+
+    if (!response.ok) {
+      throw new APIError(await response.text(), response.status);
+    }
+
+    return response;
+  }
+
+  /**
+   * Sends an HTTP request to the specified path.
+   *
+   * @async
+   * @param {string} path - The path to send the request to.
+   * @param {RequestInit} [options={}] - Optional request options.
+   * @returns {Promise<Response>} A promise that resolves to the response.
+   */
+  async request(path, options = {}) {
+    let req = new Request(new URL(path, this.#host), {
+      ...options,
+      headers: combineHeaders(defaultHeaders, options.headers ?? {}),
+    });
+
+    if (this.#requestInterceptor) {
+      req = this.#requestInterceptor(req);
+    }
+
+    return this.#fetch(req, options);
+  }
+
+  /**
+   * Sends an authenticated HTTP request to the specified path.
+   *
+   * @async
+   * @param {string} path - The path to send the request to.
+   * @param {RequestInit} [options={}] - Optional request options.
+   * @returns {Promise<Response>} A promise that resolves to the response.
+   * @throws {Error} If the account ID or token is not set.
+   */
+  async requestWithAuth(path, options = {}) {
+    const credentials = this.#ensureCredentials();
+
+    let req = new Request(new URL(path, this.#host), {
+      ...options,
+      headers: combineHeaders(
+        defaultHeaders,
+        { ...credentials },
+        options.headers ?? {}
+      ),
+    });
+
+    if (this.#requestInterceptor) {
+      req = this.#requestInterceptor(req);
+    }
+
+    return this.#fetch(req, options);
+  }
+}
---
+++ javascript/sdk/lib/types.js
@@ -0,0 +1,13 @@
+/**
+ * Represents authentication credentials.
+ *
+ * @interface Credentials
+ * @property {string} account - The account name.
+ * @property {string} token - The account token.
+ */
+
+/**
+ * @callback RequestInterceptor
+ * @param {Request} request - The original request object.
+ * @returns {Request} The modified request object.
+ */
