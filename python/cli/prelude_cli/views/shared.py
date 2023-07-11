c1dce17c0c98f7aeb9cd160578664dc1e75dbbc7
Ceci <ceci.goss@gmail.com>
2023-07-11 10:49:42 -0500

    Spinner inherits from Progress

--- python/cli/prelude_cli/views/shared.py
+++ python/cli/prelude_cli/views/shared.py
@@ -1,8 +1,6 @@
-import time
 import click
-import threading
 
-from rich.progress import Progress, SpinnerColumn, TextColumn
+from rich.progress import Progress, TextColumn, SpinnerColumn
 from functools import wraps
 
 def handle_api_error(func):
@@ -14,30 +12,13 @@ def handle_api_error(func):
             click.secho(e.args[0], fg='red')
     return handler
 
-class Spinner(): 
-    busy = False
-
-    def __init__(self, description='Loading'):
-       self.description = description
-       self.progress = Progress(
+class Spinner(Progress): 
+    def __init__(self, description='Loading'): 
+        super().__init__(
             SpinnerColumn(style='green', spinner_name='line'),
             TextColumn(f'[green]{description}'),
-            transient=True
+            transient=True, 
+            refresh_per_second=10
         )
-
-    def spinner_task(self):
-        with self.progress:
-            task = self.progress.add_task(self.description, track=False)
-            while self.busy:
-                self.progress.update(task)
-                time.sleep(0.02)
-        
-    def __enter__(self):
-        self.busy = True
-        threading.Thread(target=self.spinner_task).start()
-
-    def __exit__(self, exception, value, tb):
-        self.busy = False
-        self.progress.stop()
-        if exception is not None:
-            return False
+        task = self.add_task(description, track=False)
+        self.update(task)
