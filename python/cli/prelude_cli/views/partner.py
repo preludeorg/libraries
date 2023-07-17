5764ecf02f580c7584e4101421c55ed1fa945ac6
Ceci <ceci.goss@gmail.com>
2023-07-17 10:35:59 -0500

    Add partner code params

--- python/cli/prelude_cli/views/partner.py
+++ python/cli/prelude_cli/views/partner.py
@@ -14,41 +14,41 @@ def partner(ctx):
 
 
 @partner.command('attach')
-@click.argument('name')
+@click.argument('partner-code')
 @click.option('--api', required=True, help='API endpoint of the partner')
 @click.option('--user', default='', help='user identifier')
 @click.option('--secret', default='', help='secret for OAUTH use cases')
 @click.pass_obj
 @handle_api_error
-def attach_partner(controller, name, api, user, secret):
+def attach_partner(controller, partner_code, api, user, secret):
     """ Attach an EDR to Detect """
     with Spinner(description='Attaching partner'):
-        data = controller.attach(name=name, api=api, user=user, secret=secret)
+        data = controller.attach(partner_code=partner_code, api=api, user=user, secret=secret)
     print_json(data=data)
 
 
 @partner.command('detach')
 @click.confirmation_option(prompt='Are you sure?')
-@click.argument('name')
+@click.argument('partner-code')
 @click.pass_obj
 @handle_api_error
-def detach_partner(controller, name):
+def detach_partner(controller, partner_code):
     """ Detach an existing partner from your account """
     with Spinner(description='Detaching partner'):
-        controller.detach(name=name)
+        controller.detach(partner_code=partner_code)
 
 
 @partner.command('endpoints')
-@click.argument('name')
+@click.argument('partner-code')
 @click.option('--platform', required=True, help='platform name (e.g. "windows")', type=click.Choice(['windows', 'linux', 'darwin'], case_sensitive=False))
 @click.option('--hostname', default='', help='hostname pattern (e.g. "mycompany-c24oi444")')
 @click.option('--offset', default=0, help='API pagination offset', type=int)
 @click.pass_obj
 @handle_api_error
-def partner_endpoints(controller, name, platform, hostname, offset):
+def partner_endpoints(controller, partner_code, platform, hostname, offset):
     """ Get a list of endpoints from a partner """
     with Spinner(description='Fetching endpoints from partner'):
-        data = controller.endpoints(partner_name=name, platform=platform, hostname=hostname, offset=offset)
+        data = controller.endpoints(partner_name=partner_code, platform=platform, hostname=hostname, offset=offset)
     print_json(data=data)
 
 
