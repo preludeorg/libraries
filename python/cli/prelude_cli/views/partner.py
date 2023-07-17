66c7aee40ffa80639b2f6665c1ffb879d74a1b7e
Ceci <ceci.goss@gmail.com>
2023-07-17 11:18:10 -0500

    Import enum

--- python/cli/prelude_cli/views/partner.py
+++ python/cli/prelude_cli/views/partner.py
@@ -2,6 +2,7 @@ import click
 
 from rich import print_json
 
+from prelude_sdk.models.codes import Control
 from prelude_cli.views.shared import handle_api_error, Spinner
 from prelude_sdk.controllers.partner_controller import PartnerController
 
@@ -14,7 +15,8 @@ def partner(ctx):
 
 
 @partner.command('attach')
-@click.argument('partner-code')
+@click.argument('partner',
+              type=click.Choice([c.name.lower() for c in Control], case_sensitive=False))
 @click.option('--api', required=True, help='API endpoint of the partner')
 @click.option('--user', default='', help='user identifier')
 @click.option('--secret', default='', help='secret for OAUTH use cases')
@@ -23,23 +25,25 @@ def partner(ctx):
 def attach_partner(controller, partner_code, api, user, secret):
     """ Attach an EDR to Detect """
     with Spinner(description='Attaching partner'):
-        data = controller.attach(partner_code=partner_code, api=api, user=user, secret=secret)
+        data = controller.attach(partner_code=Control[partner_code.upper()].value, api=api, user=user, secret=secret)
     print_json(data=data)
 
 
 @partner.command('detach')
 @click.confirmation_option(prompt='Are you sure?')
-@click.argument('partner-code')
+@click.argument('partner',
+              type=click.Choice([c.name.lower() for c in Control], case_sensitive=False))
 @click.pass_obj
 @handle_api_error
 def detach_partner(controller, partner_code):
     """ Detach an existing partner from your account """
     with Spinner(description='Detaching partner'):
-        controller.detach(partner_code=partner_code)
+        controller.detach(partner_code=Control[partner_code.upper()].value)
 
 
 @partner.command('endpoints')
-@click.argument('partner-code')
+@click.argument('partner',
+              type=click.Choice([c.name.lower() for c in Control], case_sensitive=False))
 @click.option('--platform', required=True, help='platform name (e.g. "windows")', type=click.Choice(['windows', 'linux', 'darwin'], case_sensitive=False))
 @click.option('--hostname', default='', help='hostname pattern (e.g. "mycompany-c24oi444")')
 @click.option('--offset', default=0, help='API pagination offset', type=int)
@@ -48,18 +52,19 @@ def detach_partner(controller, partner_code):
 def partner_endpoints(controller, partner_code, platform, hostname, offset):
     """ Get a list of endpoints from a partner """
     with Spinner(description='Fetching endpoints from partner'):
-        data = controller.endpoints(partner_code=partner_code, platform=platform, hostname=hostname, offset=offset)
+        data = controller.endpoints(partner_code=Control[partner_code.upper()].value, platform=platform, hostname=hostname, offset=offset)
     print_json(data=data)
 
 
 @partner.command('deploy')
 @click.confirmation_option(prompt='Are you sure?')
-@click.argument('partner-code')
+@click.argument('partner',
+              type=click.Choice([c.name.lower() for c in Control], case_sensitive=False))
 @click.option('--host_ids', required=True, help='a list of host IDs to deploy to', multiple=True, default=[])
 @click.pass_obj
 @handle_api_error
 def partner_deploy(controller, partner_code, host_ids):
     """ Deploy probes to hosts associated to a partner """
     with Spinner(description='Deploying probes to hosts'):
-        data = controller.deploy(partner_code=partner_code, host_ids=host_ids)
+        data = controller.deploy(partner_code=Control[partner_code.upper()].value, host_ids=host_ids)
     print_json(data=data)
