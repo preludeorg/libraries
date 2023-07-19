22d4c540502fc0571c589daf97e301b9449d637c
stephanwampouille <stephan@preludesecurity.org>
2023-07-19 10:42:02 -0400

    Updating exit code enums

--- python/sdk/prelude_sdk/models/codes.py
+++ python/sdk/prelude_sdk/models/codes.py
@@ -46,25 +46,25 @@ class Permission(Enum):
 class ExitCode(Enum):
     MISSING = -1
     REMOVED = 0
-    ERROR = 1
-    MALFORMED_VST = 2
-    PROCESS_KILLED_1 = 9
-    PROCESS_KILLED_2 = 15
+    ERROR_OCCURED = 1
+    MALFORMED_TEST = 2
+    FORCE_KILLED = 9
+    GRACEFULLY_KILLED = 15
     PROTECTED = 100
     UNPROTECTED = 101
-    TIMEOUT = 102
-    CLEANUP_ERROR = 103
+    TIMED_OUT = 102
+    FAILED_CLEANUP = 103
     NOT_RELEVANT = 104
-    QUARANTINED_1 = 105
-    OUTBOUND_SECURE = 106
+    BEHAVIOR_QUARANTINED = 105
+    BLOCKED_AT_PERIMETER = 106
     EXPLOIT_PREVENTED = 107
-    NO_TEST = 108
+    TEST_UNAVAILABLE = 108
     IS_RELEVANT = 109
     FALSE_POSITIVE = 110
-    ENDPOINT_BLOCKED = 126
-    QUARANTINED_2 = 127
-    PROCESS_KILLED_3 = 137
-    UNEXPECTED = 256
+    EXECUTION_DISALLOWED = 126
+    FILE_QUARANTINED = 127
+    OUT_OF_MEMORY = 137
+    UNEXPECTED_ERROR = 256
 
     @classmethod
     def _missing_(cls, value):
@@ -92,31 +92,31 @@ class State(Enum):
             State.PROTECTED: [
                 ExitCode.REMOVED,
                 ExitCode.PROTECTED,
-                ExitCode.QUARANTINED_1,
-                ExitCode.QUARANTINED_2,
-                ExitCode.PROCESS_KILLED_1,
-                ExitCode.PROCESS_KILLED_2,
+                ExitCode.BEHAVIOR_QUARANTINED,
+                ExitCode.FILE_QUARANTINED,
+                ExitCode.FORCE_KILLED,
+                ExitCode.GRACEFULLY_KILLED,
                 ExitCode.NOT_RELEVANT,
-                ExitCode.NO_TEST,
-                ExitCode.OUTBOUND_SECURE,
-                ExitCode.ENDPOINT_BLOCKED,
+                ExitCode.TEST_UNAVAILABLE,
+                ExitCode.BLOCKED_AT_PERIMETER,
+                ExitCode.EXECUTION_DISALLOWED,
                 ExitCode.EXPLOIT_PREVENTED,
-                ExitCode.PROCESS_KILLED_3
+                ExitCode.OUT_OF_MEMORY
             ],
             State.UNPROTECTED: [
                 ExitCode.UNPROTECTED,
                 ExitCode.IS_RELEVANT,
             ],
             State.ERROR: [
-                ExitCode.ERROR,
-                ExitCode.MALFORMED_VST,
-                ExitCode.TIMEOUT,
-                ExitCode.UNEXPECTED,
+                ExitCode.ERROR_OCCURED,
+                ExitCode.MALFORMED_TEST,
+                ExitCode.TIMED_OUT,
+                ExitCode.UNEXPECTED_ERROR,
                 ExitCode.FALSE_POSITIVE
             ],
             State.NOT_RELEVANT: [
                 ExitCode.NOT_RELEVANT,
-                ExitCode.NO_TEST
+                ExitCode.TEST_UNAVAILABLE
             ]
         }
 
