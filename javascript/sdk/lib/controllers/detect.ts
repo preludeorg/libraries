f4052ec69b1dcf960241fc7f699cb09d372946cd
stephanwampouille <stephan@preludesecurity.org>
2023-08-02 15:01:35 -0400

    control change

--- javascript/sdk/lib/controllers/detect.ts
+++ javascript/sdk/lib/controllers/detect.ts
@@ -66,7 +66,7 @@ export default class DetectController {
         body: JSON.stringify({
           host,
           edr_id,
-          control: control !== undefined && control,
+          control,
           tags,
         }),
         ...options,
