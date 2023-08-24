8e3524785e927ae446c2108344cbf9444714d25f
privateducky <coldduck@protonmail.com>
2023-08-24 19:20:33 -0400

    updates

--- shell/probe/nocturnal.sh
+++ shell/probe/nocturnal.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
 
-PRELUDE_DIR=".vst"
-PRELUDE_CA="prelude-account-us2-us-east-1.s3.amazonaws.com"
+PRELUDE_CA="prelude-account-us1-us-west-1.s3.amazonaws.com"
 
-api="https://api.us2.preludesecurity.com"
+api="https://api.preludesecurity.com"
 dos=$(uname -s)-$(uname -m)
 
 while :
 do
-    redirect=$(curl -sfL -w '%{url_effective}' -H "token: ${PRELUDE_TOKEN}" -H "dos: ${dos}" -H "id:b74ad239-2ddd-4b1e-b608-8397a43c7c54" -H "dat: ${dat}" -H "version: 1.2" $api -o /dev/null)
+    redirect=$(curl -sfL -w '%{url_effective}' -H "token: ${PRELUDE_TOKEN}" -H "dos: ${dos}" -H "dat: ${dat}" -H "version: 1.2" $api -o /dev/null)
     test=$(echo $redirect | grep -o '[0-9a-f]\{8\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{12\}' | head -n 1)
 
     if [ $test ];then
         ca=$(echo $redirect | sed -e 's|^[^/]*//||' -e 's|/.*$||')
 
         if [ "$PRELUDE_CA" == "$ca" ];then
-            curl -sf --create-dirs -o "$PRELUDE_DIR/$test" $redirect
-            chmod +x $PRELUDE_DIR/$test && $PRELUDE_DIR/$test
+            curl -sf --create-dirs -o .vst/$test $redirect
+            chmod +x .vst/$test && .vst/$test
             code=$?
-            dat="${test}:$([[ -f $PRELUDE_DIR/$test ]] && echo $code || echo 127)"
+            dat="${test}:$([[ -f .vst/$test ]] && echo $code || echo 127)"
         fi
-    elif [[ "$location" == *"upgrade"* ]];then
+    elif [[ "$redirect" == *"upgrade"* ]];then
         echo "[P] Upgrade required" && exit 1
     else
-        rm -rf $PRELUDE_DIR
+        rm -rf .vst
         unset dat
         sleep 14440
     fi
