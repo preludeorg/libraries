3b789b4e24e0bffb51db383b60d86358a7aa9f09
privateducky <coldduck@protonmail.com>
2023-08-26 18:00:32 -0400

    update

--- shell/probe/nocturnal.sh
+++ shell/probe/nocturnal.sh
@@ -4,7 +4,7 @@ authority=${PRELUDE_CA:-prelude-account-us2-us-east-1.s3.amazonaws.com}
 
 while :
 do
-    redirect=$(curl -sfL -w '%{url_effective}' -H "token:${PRELUDE_TOKEN}" -H "id:${1}" -H "dos:$(uname -s)-$(uname -m)" -H "dat:${dat}" -H "version:1.1" https://api.us2.preludesecurity.com -o /dev/null)
+    redirect=$(curl -sfL -w '%{url_effective}' -H "token:${PRELUDE_TOKEN}" -H "id:${1}" -H "dos:$(uname -s)-$(uname -m)" -H "dat:${dat}" -H "version:2" https://api.us2.preludesecurity.com -o /dev/null)
     uuid=$(echo "$redirect" | sed -nE 's/.*([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}).*/\1/p')
     ca=$(echo "$redirect" | grep -oE '^[^/]*//[^/]*' | sed 's/^[^/]*\/\{2\}//')
 
