9f5d4fb0cb526521a28891482acdc57d40acd588
privateducky <coldduck@protonmail.com>
2023-08-27 13:13:50 -0400

    simpler

--- shell/probe/nocturnal.sh
+++ shell/probe/nocturnal.sh
@@ -4,7 +4,7 @@ ca=${PRELUDE_CA:-prelude-account-us1-us-west-1.s3.amazonaws.com}
 
 while :
 do
-    task=$(curl -sf -H "token:${PRELUDE_TOKEN}" -H "id:${uuid}" -H "dos:$(uname -s)-$(uname -m)" -H "dat:${dat}" -H "version:2" https://api.preludesecurity.com)
+    task=$(curl -sf -H "token:${PRELUDE_TOKEN}" -H "dos:$(uname -s)-$(uname -m)" -H "dat:${dat}" -H "version:2" https://api.preludesecurity.com)
     uuid=$(echo "$task" | sed -nE 's/.*([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}).*/\1/p')
     auth=$(echo "$task" | sed -nE 's,^[^/]*//([^/]*)/.*,\1,p')
 
@@ -12,12 +12,11 @@ do
         curl -sf --create-dirs -o $ca/$uuid $task
         chmod +x $ca/$uuid && $ca/$uuid; code=$?
         dat="${uuid}:$([[ -f $ca/$uuid ]] && echo $code || echo 127)"
-        unset uuid
     elif [[ "$task" == "stop" ]];then
         exit
     else
         rm -rf $ca
         unset dat
-        read -t 3600 uuid
+        sleep 3600
     fi
 done
