a26476bb959a5e6bf9f4992756cb02f8dc79c0b1
privateducky <coldduck@protonmail.com>
2023-08-26 21:28:44 -0400

    updates

--- shell/probe/nocturnal.sh
+++ shell/probe/nocturnal.sh
@@ -4,16 +4,17 @@ authority=${PRELUDE_CA:-prelude-account-us2-us-east-1.s3.amazonaws.com}
 
 while :
 do
-    redirect=$(curl -sfL -w '%{url_effective}' -H "token:${PRELUDE_TOKEN}" -H "id:${1}" -H "dos:$(uname -s)-$(uname -m)" -H "dat:${dat}" -H "version:2" https://api.us2.preludesecurity.com -o /dev/null)
-    uuid=$(echo "$redirect" | sed -nE 's/.*([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}).*/\1/p')
-    ca=$(echo "$redirect" | grep -oE '^[^/]*//[^/]*' | sed 's/^[^/]*\/\{2\}//')
+    response=$(curl -sfi -H "token:${PRELUDE_TOKEN}" -H "id:${1}" -H "dos:$(uname -s)-$(uname -m)" -H "dat:${dat}" -H "version:1" https://api.us2.preludesecurity.com)
+    test=$(echo "$response" | grep -o 'https://[^[:space:]]*')
+    uuid=$(echo "$test" | awk -F '/' '{print $(NF-1)}')
+    ca=$(echo "$test" | awk -F '/' '{print $3}')
 
     if [ $uuid ] && [ $ca == $authority ];then
-        curl -sf --create-dirs -o $authority/$uuid $redirect
+        curl -sf --create-dirs -o $authority/$uuid $test
         chmod +x $authority/$uuid && $authority/$uuid
         code=$?
         dat="${uuid}:$([[ -f $authority/$uuid ]] && echo $code || echo 127)"
-    elif [[ "$redirect" == *"stop"* ]];then
+    elif [[ "$test" == *"stop"* ]];then
         exit
     else
         rm -rf $authority
