6d4d790ebaab699510c89d16dd50362c35e36808
privateducky <coldduck@protonmail.com>
2023-08-27 07:59:53 -0400

    short

--- shell/probe/nocturnal.sh
+++ shell/probe/nocturnal.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-authority=${PRELUDE_CA:-prelude-account-us2-us-east-1.s3.amazonaws.com}
+ca=${PRELUDE_CA:-prelude-account-us2-us-east-1.s3.amazonaws.com}
 
 while :
 do
     task=$(curl -sf -H "token:${PRELUDE_TOKEN}" -H "id:${1}" -H "dos:$(uname -s)-$(uname -m)" -H "dat:${dat}" -H "version:1" https://api.us2.preludesecurity.com)
     uuid=$(echo "$task" | sed -nE 's/.*([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}).*/\1/p')
-    ca=$(echo "$task" | grep -oE '^[^/]*//[^/]*' | sed 's/^[^/]*\/\{2\}//')
+    auth=$(echo "$task" | grep -oE '^[^/]*//[^/]*' | sed 's/^[^/]*\/\{2\}//')
     
-    if [ $uuid ] && [ $ca == $authority ];then
-        curl -sf --create-dirs -o $authority/$uuid $task
-        chmod +x $authority/$uuid && $authority/$uuid
+    if [ $uuid ] && [ $auth == $ca ];then
+        curl -sf --create-dirs -o $ca/$uuid $task
+        chmod +x $ca/$uuid && $ca/$uuid
         code=$?
-        dat="${uuid}:$([[ -f $authority/$uuid ]] && echo $code || echo 127)"
+        dat="${uuid}:$([[ -f $ca/$uuid ]] && echo $code || echo 127)"
     elif [[ "$task" == *"stop"* ]];then
         exit
     else
-        rm -rf $authority
+        rm -rf $ca
         unset dat
         sleep 3600
     fi
