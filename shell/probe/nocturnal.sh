80347aae089dd274156290db97ec58e9fc9067a4
privateducky <coldduck@protonmail.com>
2023-08-27 10:35:38 -0400

    no grep

--- shell/probe/nocturnal.sh
+++ shell/probe/nocturnal.sh
@@ -6,8 +6,8 @@ while :
 do
     task=$(curl -sf -H "token:${PRELUDE_TOKEN}" -H "id:${uuid}" -H "dos:$(uname -s)-$(uname -m)" -H "dat:${dat}" -H "version:1" https://api.preludesecurity.com)
     uuid=$(echo "$task" | sed -nE 's/.*([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}).*/\1/p')
-    auth=$(echo "$task" | grep -oE '^[^/]*//[^/]*' | sed 's/^[^/]*\/\{2\}//')
-    
+    auth=$(echo "$task" | sed -nE 's,^[^/]*//([^/]*)/.*,\1,p')
+
     if [ $uuid ] && [ $auth == $ca ];then
         curl -sf --create-dirs -o $ca/$uuid $task
         chmod +x $ca/$uuid && $ca/$uuid; code=$?
