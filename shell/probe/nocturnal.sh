995e8282ac5ada33d5ff9c1ef3f0687e59ad9bab
privateducky <coldduck@protonmail.com>
2023-08-27 07:36:41 -0400

    update

--- shell/probe/nocturnal.sh
+++ shell/probe/nocturnal.sh
@@ -4,16 +4,16 @@ authority=${PRELUDE_CA:-prelude-account-us2-us-east-1.s3.amazonaws.com}
 
 while :
 do
-    redirect=$(curl -sfi -H "token:${PRELUDE_TOKEN}" -H "id:${1}" -H "dos:$(uname -s)-$(uname -m)" -H "dat:${dat}" -H "version:1" https://api.us2.preludesecurity.com | grep -i '^location:' | sed 's/Location: //i')
-    uuid=$(echo "$redirect" | sed -nE 's/.*([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}).*/\1/p')
-    ca=$(echo "$redirect" | grep -oE '^[^/]*//[^/]*' | sed 's/^[^/]*\/\{2\}//')
-
+    task=$(curl -sf -H "token:${PRELUDE_TOKEN}" -H "id:${1}" -H "dos:$(uname -s)-$(uname -m)" -H "dat:${dat}" -H "version:1" https://api.us2.preludesecurity.com)
+    uuid=$(echo "$task" | sed -nE 's/.*([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}).*/\1/p')
+    ca=$(echo "$task" | grep -oE '^[^/]*//[^/]*' | sed 's/^[^/]*\/\{2\}//')
+    
     if [ $uuid ] && [ $ca == $authority ];then
-        curl -sf --create-dirs -o $authority/$uuid $redirect
+        curl -sf --create-dirs -o $authority/$uuid $task
         chmod +x $authority/$uuid && $authority/$uuid
         code=$?
         dat="${uuid}:$([[ -f $authority/$uuid ]] && echo $code || echo 127)"
-    elif [[ "$test" == *"stop"* ]];then
+    elif [[ "$task" == *"stop"* ]];then
         exit
     else
         rm -rf $authority
