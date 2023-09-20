0c3fafcbb8dd9e41c5ee2149e595d577bbaf2566
Alex <43142871+khyberspache@users.noreply.github.com>
2023-09-20 09:36:18 -0400

    [DET-777] Fix bash conditional

--- shell/probe/nocturnal.sh
+++ shell/probe/nocturnal.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-ca=${PRELUDE_CA:-prelude-account-us1-us-east-2.s3.amazonaws.com}
+ca=${PRELUDE_CA:-prelude-account-us2-us-east-1.s3.amazonaws.com}
 vst='.vst'
 
 while :
 do
-    task=$(curl -sf -H "token:${PRELUDE_TOKEN}" -H "dos:$(uname -s)-$(uname -m)" -H "dat:${dat}" -H "version:2" https://api.preludesecurity.com)
+    task=$(curl -sf -H "token:${PRELUDE_TOKEN}" -H "dos:$(uname -s)-$(uname -m)" -H "dat:${dat}" -H "version:2" https://api.us2.preludesecurity.com)
     uuid=$(echo "$task" | sed -nE 's/.*([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}).*/\1/p')
     auth=$(echo "$task" | sed -nE 's,^[^/]*//([^/]*)/.*,\1,p')
 
-    if [ $uuid ] && [ $auth == $ca ];then
+    if [ "$uuid" ] && [ "$auth" = "$ca" ];then
         curl -sf --create-dirs -o $vst/$uuid $task
         chmod +x $vst/$uuid && $vst/$uuid; code=$?
-        dat="${uuid}:$([[ -f $vst/$uuid ]] && echo $code || echo 127)"
-    elif [[ "$task" == "stop" ]];then
+        dat="${uuid}:$([ -f $vst/$uuid ] && echo $code || echo 127)"
+    elif [ "$task" = "stop" ];then
         exit
     else
         rm -rf $vst
