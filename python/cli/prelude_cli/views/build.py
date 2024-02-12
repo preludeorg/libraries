c53f108161d695e9cfa470cb9df5ad155b094740
makk94 <mahina.kaholokula@prelude.org>
2024-02-12 08:29:38 -0800

    det-1066: CLI update from techniques to technique field

--- python/cli/prelude_cli/views/build.py
+++ python/cli/prelude_cli/views/build.py
@@ -26,11 +26,11 @@ def build(ctx):
 @click.argument('name')
 @click.option('-u', '--unit', required=True, help='unit identifier', type=str)
 @click.option('-t', '--test', help='test identifier', default=None, type=str)
-@click.option('-q', '--techniques', help='comma-separated list of valid MITRE ATT&CK codes [e.g. T1557,T1040]', default=None, type=str)
+@click.option('-q', '--technique', help='MITRE ATT&CK code [e.g. T1557]', default=None, type=str)
 @click.option('-a', '--advisory', default=None, hidden=True, type=str)
 @click.pass_obj
 @handle_api_error
-def create_test(controller, name, unit, test, techniques, advisory):
+def create_test(controller, name, unit, test, technique, advisory):
     """ Create or update a security test """
     def create_template(template, name):
         utc_time = str(datetime.now(timezone.utc))
@@ -54,7 +54,7 @@ def create_test(controller, name, unit, test, techniques, advisory):
             name=name,
             unit=unit,
             test_id=test,
-            techniques=techniques,
+            technique=technique,
             advisory=advisory
         )
 
@@ -70,18 +70,18 @@ def create_test(controller, name, unit, test, techniques, advisory):
 @click.argument('test')
 @click.option('-n', '--name', help='test name', default=None, type=str)
 @click.option('-u', '--unit', help='unit identifier', default=None, type=str)
-@click.option('-q', '--techniques', help='comma-separated list of valid MITRE ATT&CK codes [e.g. T1557,T1040]', default=None, type=str)
+@click.option('-q', '--technique', help='MITRE ATT&CK code [e.g. T1557]', default=None, type=str)
 @click.option('-a', '--advisory', help='alert identifier [CVE ID, Advisory ID, etc]', default=None, hidden=True, type=str)
 @click.pass_obj
 @handle_api_error
-def update_test(controller, test, name, unit, techniques, advisory):
+def update_test(controller, test, name, unit, technique, advisory):
     """ Create or update a security test """
     with Spinner(description='Updating test'):
         data = controller.update_test(
             test_id=test,
             name=name,
             unit=unit,
-            techniques=techniques,
+            technique=technique,
             advisory=advisory
         )
     print_json(data=data)
