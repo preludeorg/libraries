433294b8a16dee67d3609a057f37d02b0e2fc534
Patrick Flaherty <patrick@preludesecurity.com>
2024-05-21 18:06:00 -0400

    output timeout

--- shell/probe/raindrop.ps1
+++ shell/probe/raindrop.ps1
@@ -9,10 +9,9 @@ function Execute {
         $proc = Start-Process -FilePath $File -NoNewWindow -PassThru -RedirectStandardOutput $stdoutTempFile -RedirectStandardError $stderrTempFile
 
         $errVar = $null
-        $proc | Wait-Process -Timeout 45 -ErrorAction SilentlyContinue -ErrorVariable errVar
+        $proc | Wait-Process -Timeout 10 -ErrorAction SilentlyContinue -ErrorVariable timeoutVar
 
-        if ($errVar) {
-            Write-Host $errVar
+        if ($timeoutVar) {
             $proc | kill
             return 102
         }
@@ -30,6 +29,7 @@ function Execute {
         $stdout = if ($stdout) { [string]::Join("; ", $stdout) } else { "" }
         $stderr = Get-Content -Path $stderrTempFile
         $stderr = if ($stderr) { [string]::Join("; ", $stderr) } else { "" }
+        if ($timeoutVar) {$stdErr = [string]::Join(" ", $stderr, "TEST TIMEOUT; ") } 
         if ($stdout -or $stderr) {
             Write-Host "${stdout}; ${stderr}"
         }
@@ -41,13 +41,13 @@ function FromEnv { param ([string]$envVar, [string]$default)
     if ($envVal) { return $envVal } else { return $default }
 }
 
-$ca = FromEnv "PRELUDE_CA" "prelude-account-us1-us-east-2.s3.amazonaws.com"
+$ca = FromEnv "PRELUDE_CA" "prelude-account-us2-us-east-1.s3.amazonaws.com"
 $dir = FromEnv "PRELUDE_DIR" ".vst"
 $dat = ""
 
 while ($true) {
     try {
-        $task = Invoke-WebRequest -Uri "https://api.preludesecurity.com" -Headers @{
+        $task = Invoke-WebRequest -Uri "https://api.us2.preludesecurity.com" -Headers @{
             "token" = $env:PRELUDE_TOKEN
             "dos" = "windows-$Env:PROCESSOR_ARCHITECTURE"
             "dat" = $dat
