cecab1cdea8028f17e14ad37c61867b3f28d87ec
Patrick Flaherty <patrick@preludesecurity.com>
2024-05-29 18:20:10 -0400

    DET-1602: dash compatibility (#1028)

--- shell/probe/nocturnal.sh
+++ shell/probe/nocturnal.sh
@@ -27,7 +27,7 @@ do
           sleep 1
           elapsed_time=$((elapsed_time + 1))
         done
-        if [[ $code -ne 102 ]]; then
+        if [ $code -ne 102 ]; then
           wait $test_pid
           code=$?
         fi
