4974437b150fb7d5c00b3a406996a707cba54e3d
garjarbinks <garreth@preludesecurity.com>
2024-07-25 14:42:46 -0400

    Logging for Raindrop
    Create Log function to write output to host/output and to append file
    Create LogRotate function to handle number of logs
    Create additional, optional, Logging variables (PRELUDE_LOGDIR, PRELUDE_LOGCOUNT, PRELUDE_LOGREADEVERYONE)
    
    example output:
    Prelude probe: version 2.5
    System.Management.Automation.RuntimeException: Test cycle done
    Prelude probe: version 2.5
    Downloading 2912bc1e-c5cc-487a-8281-dc1986442135
    Invoking 2912bc1e-c5cc-487a-8281-dc1986442135
    [2024-07-25T11:40:04][2912bc1e-c5cc-487a-8281-dc1986442135] Starting test at: 2024-07-25T11:40:04
    [2024-07-25T11:40:04][2912bc1e-c5cc-487a-8281-dc1986442135] Extracting file for quarantine test
    [2024-07-25T11:40:04][2912bc1e-c5cc-487a-8281-dc1986442135] Pausing for 3 seconds to gauge defensive reaction
    [2024-07-25T11:40:07][2912bc1e-c5cc-487a-8281-dc1986442135] Malicious file was not caught
    [2024-07-25T11:40:07][2912bc1e-c5cc-487a-8281-dc1986442135] Creating LNK file
    [2024-07-25T11:40:08][2912bc1e-c5cc-487a-8281-dc1986442135] Completed with code: 101
    [2024-07-25T11:40:08][2912bc1e-c5cc-487a-8281-dc1986442135] Exit called from line: 48
    [2024-07-25T11:40:08][2912bc1e-c5cc-487a-8281-dc1986442135] Ending test at: 2024-07-25T11:40:08
    Done Invoking 2912bc1e-c5cc-487a-8281-dc1986442135:101
    System.Management.Automation.RuntimeException: Test cycle done

--- shell/probe/raindrop.ps1
+++ shell/probe/raindrop.ps1
@@ -18,19 +18,24 @@ function Execute {
         $code = if (Test-Path $File) {$proc.ExitCode} Else {127}
         return $code
     } catch [System.UnauthorizedAccessException] {
+        Log "Exception: UnauthorizedAccessException - $($_.Exception.Message)"
         return 126
     } catch [System.InvalidOperationException] {
+        Log "Exception: InvalidOperationException - $($_.Exception.Message)"
         return 127
     } catch {
+        Log "Exception: GenericException - $($_.Exception.Message)"
         return 1
     } finally {
-        $stdout = Get-Content -Path $stdoutTempFile
-        $stdout = if ($stdout) { [string]::Join("; ", $stdout) } else { "" }
-        $stderr = Get-Content -Path $stderrTempFile
-        $stderr = if ($stderr) { [string]::Join("; ", $stderr) } else { "" }
-
-        if ($stdout -or $stderr -or $timeoutVar) {
-            Write-Host "${stdout}; ${stderr}; ${timeoutVar}"
+        $logMessageArray = @()
+        $stdoutLines = Get-Content -Path $stdoutTempFile | Where-Object { $_ -ne "" }
+        if ($stdoutLines) { $logMessageArray += [string]::Join("`n", $stdoutLines) }
+        $stderrLines = Get-Content -Path $stderrTempFile | Where-Object { $_ -ne "" }
+        if ($stderrLines) { $logMessageArray += [string]::Join("`n", $stderrLines) }
+        if ($timeoutVar) { $logMessageArray += $timeoutVar }
+        if ($logMessageArray) {
+            $logMessageString = [string]::Join("`n", $logMessageArray)
+            Log $logMessageString
         }
     }
 }
@@ -40,15 +45,49 @@ function FromEnv { param ([string]$envVar, [string]$default)
     if ($envVal) { return $envVal } else { return $default }
 }
 
+function Log { param ([string]$message, [bool]$hostonly = $true)
+    if ($host) { Write-Host $message }
+    else { Write-Output $message }
+    if ($logFileCount -gt 0) { Add-Content -Path $LogFile -Value $message }
+}
+
+function RotateLogs {
+    $logFiles = Get-ChildItem -Path $logDir -Filter "$baseLogFileName-*.log" | Sort-Object LastWriteTime -Descending
+
+    if ($logFiles.Count -gt $logFileCount) {
+        Log "Rotating Logs"
+        $logFiles[$logFileCount..($logFiles.Count - 1)] | ForEach-Object { Remove-Item $_.FullName }
+        $global:LogFile = Get-LogFileName
+        Log "Logs Rotated"
+        if ($logEveryoneRead) { icacls $logDir /grant "Everyone:R" /t /c | Out-Null }
+    }
+    $global:LogFile = Get-LogFileName
+}
+
+function Get-LogFileName {
+    return Join-Path $logDir "$baseLogFileName-$(Get-Date -Format 'yyyyMMdd').log"
+}
+
 $ca = FromEnv "PRELUDE_CA" "prelude-account-us1-us-east-2.s3.amazonaws.com"
-$dir = FromEnv "PRELUDE_DIR" ".vst"
+$dir = FromEnv "PRELUDE_DIR" (Join-Path (Split-Path -Parent $MyInvocation.MyCommand.Path) ".vst")
 $dat = ""
 $version = "2.5"
+$logDir = FromEnv "PRELUDE_LOGDIR" (Join-Path (Split-Path -Parent $dir) "logs")
+$baseLogFileName = "LogFile"
+$logFileCount = [int](FromEnv "PRELUDE_LOGCOUNT" 7)
+$LogFile = Get-LogFileName
+$logEveryoneRead = (FromEnv "PRELUDE_LOGREADEVERYONE" "false") -imatch "^(1|yes|true)$"
 
-Write-Output "Prelude probe: version ${version}"
+if (-not (Test-Path $logDir)) {
+    New-Item -Path $logDir -ItemType Directory | Out-Null
+}
+if ($logEveryoneRead) { icacls $logDir /grant "Everyone:R" /t /c | Out-Null }
+
+Log "Prelude probe: version ${version}" $false
 
 while ($true) {
     try {
+        RotateLogs
         $task = Invoke-WebRequest -Uri "https://api.preludesecurity.com" -Headers @{
             "token" = $env:PRELUDE_TOKEN
             "dos" = "windows-$Env:PROCESSOR_ARCHITECTURE"
@@ -60,20 +99,23 @@ while ($true) {
         $auth = $task.content -replace '^[^/]*//([^/]*)/.*', '$1'
 
         if ($uuid -and $auth -eq $ca) {
+            Log "Downloading $uuid"
             Invoke-WebRequest -Uri $task.content -OutFile (New-Item -path "$dir\$uuid.exe" -Force ) -UseBasicParsing
+            Log "Invoking $uuid"
             $code = Execute "$dir\$uuid.exe"
             $dat = "${uuid}:${code}"
+            Log "Done Invoking $dat"
         } elseif ($task.content -eq "stop") {
             exit
         } else {
             throw "Test cycle done"
         }
     } catch {
-        Write-Output $_.Exception
+        Log $_.Exception $false
         Remove-Item $dir -Force -Recurse -ErrorAction SilentlyContinue
         $dat = ""
         if (-Not ($task.content -as [int])) {
-            Write-Output "Invalid sleep time: $task"
+            Log "Invalid sleep time: $task" $false
             $task = 1800
         }
         Start-Sleep -Seconds "$task"
