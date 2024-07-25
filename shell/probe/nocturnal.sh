fdf94df6c3d13d9d26d0fdf0cc760b3ba0b5ed26
garjarbinks <garreth@preludesecurity.com>
2024-07-25 14:44:13 -0400

    use cd instead of pushd popd

--- shell/probe/nocturnal.sh
+++ shell/probe/nocturnal.sh
@@ -13,11 +13,14 @@ do
     auth=$(echo "$task" | sed -nE 's,^[^/]*//([^/]*)/.*,\1,p')
 
     if [ "$uuid" ] && [ "$auth" = "$ca" ];then
+        echo "Downloading $uuid"
         curl -sf --max-redirs 0 --create-dirs -o "$vst/$uuid" $task
         chmod +x "$vst/$uuid"
-        pushd "$vst" &>/dev/null
+        CDIR="${PWD}"
+        cd "$vst" &>/dev/null
+        echo "Invoking $uuid"
         $vst/$uuid & test_pid=$!
-        popd &>/dev/null
+        cd "$CDIR" &>/dev/null
         elapsed_time=0
         while kill -0 $test_pid 2> /dev/null; do
           if [ $elapsed_time -ge 45 ]; then
@@ -34,6 +37,7 @@ do
           code=$?
         fi
         dat="${uuid}:$([ -f "$vst/$uuid" ] && echo $code || echo 127)"
+        echo "Done Invoking $dat"
         unset code
     elif [ "$task" = "stop" ];then
         exit
