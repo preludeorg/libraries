4013a477b6022f36bb475f4b989161a6cde956fb
Patrick Flaherty <patrick@preludesecurity.com>
2024-09-03 17:41:45 -0400

    Nocturnal should launch the same in and outside of containers

--- shell/probe/vision.sh
+++ shell/probe/vision.sh
@@ -2,9 +2,8 @@
 
 dos=$(uname -s)-$(uname -m)
 dir=/opt/preludesecurity
-mkdir -p "${dir}/.vst"
+mkdir -p "$dir"
 curl -sL "https://api.preludesecurity.com/download/nocturnal" -H "dos:${dos}" > "${dir}/nocturnal"
 chmod +x "${dir}/nocturnal"
-cd "${dir}/.vst" &>/dev/null
 "${dir}/nocturnal" > "${dir}/detect.log" 2>&1 &
 exec /bin/sh -c "$*"
