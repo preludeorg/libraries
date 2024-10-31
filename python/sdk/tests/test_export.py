12db7247f1849c949eabdf26132839f93f3b598c
Tanya Tang <tanya@preludesecurity.com>
2024-10-31 09:46:59 -0400

    update to match us2

--- python/sdk/tests/test_export.py
+++ python/sdk/tests/test_export.py
@@ -10,6 +10,6 @@ class TestExport:
     def setup_class(self):
         self.export = ExportController(pytest.account)
 
-    def test_export_missing_control_csv(self, unwrap):
-        export_url = unwrap(self.export.partner)(self.export, 'missing_control')
+    def test_export_missing_edr_endpoints_csv(self, unwrap):
+        export_url = unwrap(self.export.partner)(self.export, 'endpoints?$filter=not known_by_edrs/any()')
         assert 'url' in export_url
