9d279339b2d99d682411437ebf7cc3d5822df21e
Tanya Tang <tanya@preludesecurity.com>
2024-11-04 13:10:32 -0500

    Add email provider to sdk tests (#1114)

--- python/sdk/tests/test_policy_evaluator.py
+++ python/sdk/tests/test_policy_evaluator.py
@@ -21,13 +21,16 @@ class TestPolicyEvaluationSummary:
         if not pytest.expected_account['features']['policy_evaluator']:
             pytest.skip('POLICY_EVALUATOR feature not enabled')
         summary = unwrap(self.partner.get_policy_evaluation_summary)(self.partner)
-        assert {'endpoint_summary', 'user_summary'} == summary.keys()
+        assert {'endpoint_summary', 'user_summary', 'inbox_summary'} == summary.keys()
         assert {'controls', 'missing_edr_count', 'total_endpoint_count'} == summary['endpoint_summary'].keys()
         for control_summary in summary['endpoint_summary']['controls']:
             assert {'control', 'endpoint_count', 'policy_conflict_count', 'setting_misconfiguration_count'} == control_summary.keys()
         assert {'controls', 'total_user_count'} == summary['user_summary'].keys()
         for control_summary in summary['user_summary']['controls']:
             assert {'control', 'user_count', 'setting_misconfiguration_count'} == control_summary.keys()
+        assert {'controls', 'total_inbox_count'} == summary['inbox_summary'].keys()
+        for control_summary in summary['inbox_summary']['controls']:
+            assert {'control', 'inbox_control', 'setting_misconfiguration_count'} == control_summary.keys()
 
 @pytest.mark.order(10)
 @pytest.mark.usefixtures('setup_account')
@@ -37,7 +40,6 @@ class TestPolicyEvaluation:
         ('defender', True),
         ('sentinelone', True),
         ('intune', False),
-        ('servicenow', False),
     ]
 
     def setup_class(self):
@@ -60,7 +62,10 @@ class TestPolicyEvaluation:
             else:
                 assert len(evaluation['policies']) == 0
             assert {'no_av_policy_count', 'no_edr_policy_count', 'missing_control_count', 'reduced_functionality_mode_count'} == evaluation['misconfigured'].keys()
-        if 'user_evaulation' in evaluation:
-            evaluation = evaluation['user_evaulation']
+        if 'user_evaluation' in evaluation:
+            evaluation = evaluation['user_evaluation']
             assert {'policies', 'misconfigured', 'total_user_count'} == evaluation.keys()
             assert {'missing_mfa_count'} == evaluation['misconfigured'].keys()
+        if 'inbox_evaluation' in evaluation:
+            evaluation = evaluation['inbox_evaluation']
+            assert {'policies, total_inbox_count'} == evaluation.keys()
