8d458f65b23fdb19a7ab26eaaecf8ac7be1a5c24
Mahina Kaholokula <mahina.kaholokula@prelude.org>
2024-11-08 13:06:57 -0500

    det-2211: add mappings from control to category (#1119)

--- python/sdk/prelude_sdk/models/codes.py
+++ python/sdk/prelude_sdk/models/codes.py
@@ -176,17 +176,49 @@ class Control(Enum, metaclass=MissingItem):
     def _missing_(cls, value):
         return Control.INVALID
 
+    @property
+    def category(self):
+        for k, v in ControlCategory.mapping().items():
+            if self in v:
+                return k
+        return ControlCategory.NONE
+
 class ControlCategory(Enum, metaclass=MissingItem):
     INVALID = -1
+    NONE = 0
     CLOUD = 1
     EMAIL = 2
     IDENTITY = 3
     NETWORK = 4
     XDR = 5
+    ASSET_MANAGER = 6
 
     @classmethod
     def _missing_(cls, value):
         return ControlCategory.INVALID
+
+    @classmethod
+    def mapping(cls):
+        return {
+            ControlCategory.ASSET_MANAGER: [
+                Control.INTUNE,
+                Control.SERVICENOW,
+            ],
+            ControlCategory.CLOUD: [],
+            ControlCategory.EMAIL: [
+                Control.M365,
+            ],
+            ControlCategory.IDENTITY: [
+                Control.ENTRA,
+                Control.OKTA,
+            ],
+            ControlCategory.NETWORK: [],
+            ControlCategory.XDR: [
+                Control.CROWDSTRIKE,
+                Control.DEFENDER,
+                Control.SENTINELONE,
+            ],
+        }
 
 class PartnerSyncJobType(Enum, metaclass=MissingItem):
     INVALID = -1
