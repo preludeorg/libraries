b9d7d93d94993b1a171b3aeeef98cba62d1d77dd
Tanya Tang <tanya@preludesecurity.com>
2024-12-03 10:56:46 -0500

    fix

--- python/sdk/prelude_sdk/models/codes.py
+++ python/sdk/prelude_sdk/models/codes.py
@@ -226,7 +226,7 @@ class BackgroundJobTypes(Enum, metaclass=MissingItem):
     DEPLOY_PROBE = 2
     OBSERVED_DETECTED = 3
     PRELUDE_ENDPOINT_SYNC = 4
-    EXPORT = 5
+    EXPORT_SCM = 5
 
     @classmethod
     def _missing_(cls, value):
