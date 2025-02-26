5093a2be9087484ae350a27bd46a491be54c981a
Tanya Tang <tanya@preludesecurity.com>
2025-02-26 17:56:31 -0500

    Det 2529: refresh token (#1159)

--- python/sdk/prelude_sdk/controllers/generate_controller.py
+++ python/sdk/prelude_sdk/controllers/generate_controller.py
@@ -1,11 +1,10 @@
 from prelude_sdk.controllers.http_controller import HttpController
-
-from prelude_sdk.models.account import verify_credentials
+from prelude_sdk.models.account import Account, verify_credentials
 from prelude_sdk.models.codes import Control
 
 
 class GenerateController(HttpController):
-    def __init__(self, account):
+    def __init__(self, account: Account):
         super().__init__()
         self.account = account
 
