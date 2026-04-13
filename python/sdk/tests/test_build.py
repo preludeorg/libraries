05ba3c605518602fc13eeb0130bc728e88a3a591
Stephan W <stephan@preludesecurity.com>
2026-04-13 12:52:29 -0400

    det-4098: update VST tests to check for updated

--- python/sdk/tests/test_build.py
+++ python/sdk/tests/test_build.py
@@ -40,6 +40,7 @@ class TestVST:
 
         diffs = check_dict_items(expected, pytest.expected_test)
         assert not diffs, json.dumps(diffs, indent=2)
+        assert pytest.expected_test["updated"] == pytest.expected_test["created"]
 
     def test_upload(self, unwrap):
         def wait_for_compile(job_id):
@@ -136,9 +137,14 @@ class TestVST:
         pytest.expected_test["expected"]["crowdstrike"] = EDRResponse.PREVENT.value
         pytest.expected_test["name"] = updated_name
         pytest.expected_test["technique"] = "T1234.001"
+        pytest.expected_test["updated"] = res["updated"]
 
         diffs = check_dict_items(pytest.expected_test, res)
         assert not diffs, json.dumps(diffs, indent=2)
+
+        assert parse(res["updated"]).replace(tzinfo=timezone.utc) <= datetime.now(
+            timezone.utc
+        ) + timedelta(minutes=1)
 
     def test_download(self, unwrap):
         filename = f"{pytest.test_id}.go"
